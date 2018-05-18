fe6a351b84dd102c807e8bd84ae5cf820f6274c6
Sean Rumsby <sean@olivewoodsoftware.com>
2018-05-18 16:29:30 -0700

    Added localhost config settings

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -34,5 +34,19 @@ module.exports = {
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
     cssSourceMap: false
+  },
+  localhost: {
+    env: require('./localhost.env'),
+    port: 8080,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    // CSS Sourcemaps off by default because relative paths are "buggy"
+    // with this option, according to the CSS-Loader README
+    // (https://github.com/webpack/css-loader#sourcemaps)
+    // In our experience, they generally work as expected,
+    // just be aware of this issue when enabling this option.
+    cssSourceMap: false
   }
 }
