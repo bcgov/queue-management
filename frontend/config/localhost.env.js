a8bd2bef6f16f59f93892ebf2f2ba5c59ca001e2
Sean Rumsby <sean@olivewoodsoftware.com>
2018-05-18 16:28:45 -0700

    Added environmental configs

--- frontend/config/localhost.env.js
+++ frontend/config/localhost.env.js
@@ -2,6 +2,6 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_URL: '"https://servicebc-cfms-dev.pathfinder.gov.bc.ca/"'
+  NODE_ENV: '"localhost"',
+  API_URL: '"http://localhost:5000"'
 })
