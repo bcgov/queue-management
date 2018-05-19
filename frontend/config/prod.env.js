b5bab60fcfbabc21fb60338651f76e8df70ee5cc
Sean Rumsby <sean@olivewoodsoftware.com>
2018-05-19 10:35:19 -0700

    Remove the trailing slash from all base api_url configs

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,4 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://api-servicebc-cfms-dev.pathfinder.gov.bc.ca/"'
+  API_URL: '"https://api-servicebc-cfms-dev.pathfinder.gov.bc.ca"'
 }
