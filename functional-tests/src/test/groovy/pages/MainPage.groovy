d0604e8b71ddc663dbea93fae1fd566086f4e3ab
Sean Rumsby <sean@olivewoodsoftware.com>
2018-05-20 17:32:54 -0700

    More updates to the functional tests

--- functional-tests/src/test/groovy/pages/MainPage.groovy
+++ functional-tests/src/test/groovy/pages/MainPage.groovy
@@ -4,9 +4,7 @@ import geb.Page
 class MainPage extends Page {
     static at = { title == "Queue Management Proof of Concept" }
     static content = {
-        UsernameInput { $('input[id = "login-username"]') }
-        PasswordInput { $('input[id = "login-password"]') }
-        LoginButton { $('button[id = "login-button"]') }
+        
     }
 
     Boolean login(String username, String password) {
