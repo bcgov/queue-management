c3d7b6586718f6b2273f6a549bc6add96cd535fd
Sean Rumsby <sean@olivewoodsoftware.com>
2018-05-25 15:28:04 -0700

    Added support for database engine

--- api/config.py
+++ api/config.py
@@ -44,18 +44,19 @@ class DevelopmentConfig(BaseConfig):
     ]
     USE_HTTPS = True
 
-    # POSTGRESQL
+    DB_ENGINE = os.getenv('DATABASE_ENGINE', 'mysql')
     DB_USER = os.getenv('DATABASE_USERNAME', '')
     DB_PASSWORD = os.getenv('DATABASE_PASSWORD','')
     DB_NAME = os.getenv('DATABASE_NAME','')
     DB_HOST = os.getenv('DATABASE_HOST','')
-    DB_PORT = os.getenv('DATABASE_PORT','5432')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
-         user=DB_USER,
-         password=DB_PASSWORD,
-         host=DB_HOST,
-         port=int(DB_PORT),
-         name=DB_NAME,
+    DB_PORT = os.getenv('DATABASE_PORT','3306')
+    SQLALCHEMY_DATABASE_URI = '{engine}://{user}:{password}@{host}:{port}/{name}'.format(
+        engine=DB_ENGINE,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=int(DB_PORT),
+        name=DB_NAME,
     )
 
 def configure_app(app):
