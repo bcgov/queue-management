a71460b1adcdd172b66e3a052d0f80f56d45e9c0
Sean Rumsby <sean@olivewoodsoftware.com>
2018-05-25 14:08:48 -0700

    Add event for joining a room

--- api/app/resources/websocket.py
+++ api/app/resources/websocket.py
@@ -1,4 +1,5 @@
-from flask_socketio import emit
+from flask_login import current_user
+from flask_socketio import emit, join_room
 
 from app.models import Client
 from qsystem import socketio
@@ -6,3 +7,11 @@ from qsystem import socketio
 @socketio.on('my event')
 def test_message(message):
     emit('my response', {'data': 'got it!', 'count': message['count']})
+
+@socketio.on('join')
+def join_room(message):
+    if current_user.is_authenticated:
+        join_room(current_user.office_id)
+        emit('joined', {'data': "{user} has joined the room".format(user=current_user.username)})
+    else:
+    	emit('join_fail', {'data': "{user} failed to join the room".format(user=current_user.username)})
