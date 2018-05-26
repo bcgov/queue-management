ce48be59d18d4261e8b2fde971d49782f279f944
Sean Rumsby <sean@olivewoodsoftware.com>
2018-05-25 17:39:01 -0700

    Testing

--- api/app/resources/websocket.py
+++ api/app/resources/websocket.py
@@ -14,9 +14,10 @@ def test_message(message):
 @socketio.on('joinRoom')
 def join_room(message):    
     print("Received request to join room")
+    print(message)
     if current_user.is_authenticated:
         print("{user} is attempting to join room: {room}".format(user=current_user.username, room=current_user.office_id))
-        join_room(message['office_id'])
+        join_room("vancouver")
         emit('joinRoomSuccess', {'data': "{user} has joined the room".format(user=current_user.username)})
     else:
         print ("Join failed as user is not authed")
