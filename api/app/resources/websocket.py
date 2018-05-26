45531c763ca70a83cba803e57ddbc0c965842013
Sean Rumsby <sean@olivewoodsoftware.com>
2018-05-25 17:16:04 -0700

    Continue to work on webscoket

--- api/app/resources/websocket.py
+++ api/app/resources/websocket.py
@@ -4,20 +4,20 @@ from flask_socketio import emit, join_room
 from app.models import Client
 from qsystem import socketio
 
-@socketio.on('my event')
+@socketio.on('myEvent')
 def test_message(message):
     print("Receiving message")
     print(message)
-    emit('my response', {'data': 'got it!', 'count': message['count']})
+    emit('myResponse', {'data': 'got it!', 'count': message['count']})
     print("Pinging back")
 
-@socketio.on('join')
+@socketio.on('joinRoom')
 def join_room(message):    
     print("Received request to join room")
     if current_user.is_authenticated:
         print("{user} is attempting to join room: {room}".format(user=current_user.username, room=current_user.office_id))
-        join_room(current_user.office_id)
-        emit('joined', {'data': "{user} has joined the room".format(user=current_user.username)})
+        join_room(message['office_id'])
+        emit('joinRoomSuccess', {'data': "{user} has joined the room".format(user=current_user.username)})
     else:
         print ("Join failed as user is not authed")
-        emit('join_fail', {'data': "{user} failed to join the room".format(user=current_user.username)})
+        emit('joinRoomFail', {'data': "{user} failed to join the room".format(user=current_user.username)})
