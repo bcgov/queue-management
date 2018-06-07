a645c68dc0bdd6bbac32bcda9e098a745d76e8cc
Adam Kroon <adam.kroon@viryltech.com>
2018-06-06 21:12:10 -0700

    #5 Create models for all objects - styling changes, spaces, tabs, characters per line

--- api/app/models/CSRTable.py
+++ api/app/models/CSRTable.py
@@ -4,44 +4,49 @@ from base import Base
 
 class CSRTable(MyBase, Base, db.model):
 
-	model = api.model ('CSRTable' {
-		'csr_id' : fields.Integer,
-		'username' : fields.String,
-		'password' : fields.String,
-		'office_id_in_now' : fields.Integer,
-		'role_id' : fields.String,
-		'qt_xn_csr_now' : fields.Integer,
-		'receptionist_now' : fields.Integer,
-		'deleted' : fields.String,
-		'csr_state_id_now' : fields.Integer
-		})
+    model = api.model ('CSRTable' {
+        'csr_id' : fields.Integer,
+        'username' : fields.String,
+        'password' : fields.String,
+        'office_id_in_now' : fields.Integer,
+        'role_id' : fields.String,
+        'qt_xn_csr_now' : fields.Integer,
+        'receptionist_now' : fields.Integer,
+        'deleted' : fields.String,
+        'csr_state_id_now' : fields.Integer
+    })
 
-	csr_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-	username = db.Column(db.String(150))
-	password = db.Column(db.String(45), nullable=False)
-	# TODO check FK for Office Table
-	office_id_in_now = db.Column(db.BigInteger)
-	# TODO check FK for Role Table
-	role_id = db.Column(db.BigInteger)
-	qt_xn_csr_now = db.Column(db.Binary)
-	receptionist_now = db.Column(db.Integer)
-	deleted = db.Column(db.DateTime, nullable=True)
-	csr_state_id_now = db.Column(db.BigInteger)
+    csr_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(150))
+    password = db.Column(db.String(45), nullable=False)
+    # TODO check FK for Office Table
+    office_id_in_now = db.Column(db.BigInteger)
+    # TODO check FK for Role Table
+    role_id = db.Column(db.BigInteger)
+    qt_xn_csr_now = db.Column(db.Binary)
+    receptionist_now = db.Column(db.Integer)
+    deleted = db.Column(db.DateTime, nullable=True)
+    csr_state_id_now = db.Column(db.BigInteger)
 
-	def __repr__(self, username):
-		return '<CSR User: %r>' % self.username
+    def __repr__(self, username):
+        return '<CSR User: %r>' % self.username
 
-	def __init__ (self, csr_id, username, password, office_id_in_now, role_id, qt_xn_csr_now, receptionist_now, deleted, csr_state_id_now):
-		self.csr_id = csr_id
-		self.username = username
-		self.password = password
-		self.office_id_in_now = office_id_in_now
-		self.role_id = role_id
-		self.qt_xn_csr_now = qt_xn_csr_now
-		self.receptionist_now = receptionist_now
-		self.deleted = deleted
-		self.csr_state_id_now = csr_state_id_now
+    def __init__ (self, csr_id, username, password, office_id_in_now, role_id, qt_xn_csr_now, receptionist_now, deleted,
+                 csr_state_id_now):
+        self.csr_id = csr_id
+        self.username = username
+        self.password = password
+        self.office_id_in_now = office_id_in_now
+        self.role_id = role_id
+        self.qt_xn_csr_now = qt_xn_csr_now
+        self.receptionist_now = receptionist_now
+        self.deleted = deleted
+        self.csr_state_id_now = csr_state_id_now
 
-	# TODO Check normal line length for large return values 
-	def json(self, csr_id, username, password, office_id_in_now, role_id, qt_xn_csr_now, receptionist_now, deleted, csr_state_id_now):
-		return {"csr_id" : self.csr_id, "username" : self.username, "password" : self.password, "office_id_in_now" : office_id_in_now, "role_id" : self.role_id, "qt_xn_csr_now" : self.qt_xn_csr_now, "receptionist_now" : self.receptionist_now, "deleted" : self.deleted, "csr_state_id_now" : self.csr_state_id_now}
+    # TODO Check normal line length for large return values 
+    def json(self, csr_id, username, password, office_id_in_now, role_id, qt_xn_csr_now, receptionist_now, deleted, 
+            csr_state_id_now):
+        return {"csr_id" : self.csr_id, "username" : self.username, "password" : self.password, 
+                "office_id_in_now" : office_id_in_now, "role_id" : self.role_id, "qt_xn_csr_now" : self.qt_xn_csr_now, 
+                "receptionist_now" : self.receptionist_now, "deleted" : self.deleted, 
+                "csr_state_id_now" : self.csr_state_id_now}
