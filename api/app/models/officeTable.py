71f92e3616dd43347918fef7ac56adceaf61cfcb
Adam Kroon <adam.kroon@viryltech.com>
2018-06-06 21:12:42 -0700

    #5 Create models for all objects - styling changes, spaces, tabs, characters per line

--- api/app/models/officeTable.py
+++ api/app/models/officeTable.py
@@ -4,17 +4,17 @@ from base import Base
 
 class OfficeTable(MyBase, Base, db.model):
 
-	model = api.model('OfficeTable' {
-		'office_id' : fields.Integer,
-		'office_name' : fields.String,
-		'sb_id' : fields.Integer,
-		'deleted' : fields.String
-		})
+    model = api.model('OfficeTable' {
+        'office_id' : fields.Integer,
+        'office_name' : fields.String,
+        'sb_id' : fields.Integer,
+        'deleted' : fields.String
+    })
 
-	office_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-	office_name = db.Column(db.String(100))
-	sb_id = db.Column(db.BigInteger)
-	deleted = db.Column(db.DateTime, nullable=True)
+    office_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    office_name = db.Column(db.String(100))
+    sb_id = db.Column(db.BigInteger)
+    deleted = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self, office_name):
         return '<Office: %r>' % self.office_name
@@ -26,4 +26,5 @@ class OfficeTable(MyBase, Base, db.model):
         self.deleted = deleted
 
     def json(self, office_id, office_name, sb_id, deleted):
-        return {"office_id" : self.office_id, "office_name" : self.office_name, "sb_id" : self.sb_id, "deleted" : self.deleted }
+        return {"office_id" : self.office_id, "office_name" : self.office_name, "sb_id" : self.sb_id, 
+                "deleted" : self.deleted }
