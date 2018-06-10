13d93e557e8620403adef87e36de577c97a22070
Adam Kroon <adam.kroon@viryltech.com>
2018-06-09 21:38:34 -0700

    #5 Create models for all objects - citizenStateTable model created

---
+++ api/app/models/citizenStateTable.py
@@ -0,0 +1,26 @@
+from flask_restplus import fields
+from qsystem import api, db
+from base import Base 
+
+class CitizenStateTable(MyBase, Base, db.model):
+
+    model = api.model('CitizenStateTable' {
+        'cs_id' = fields.Integer,
+        'cs_state_name' = fields.String,
+        'cs_state_desc' = fields.String
+        })
+
+    cs_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    cs_state_name = db.Column(db.String(100))
+    cs_state_desc = db.Column(db.String(1000))
+
+    def __repr__(self, cs_state_name):
+        return '<CS State Name: %r>' % self.cs_state_name
+
+    def __init__(self, cs_id, cs_state_name, cs_state_desc):
+        self.cs_id = cs_id
+        self.cs_state_name = cs_state_name
+        self.cs_state_desc = cs_state_desc
+
+    def json(self, cs_id, cs_state_name, cs_state_desc)
+        return {"cs_id" : self.cs_id, "cs_state_name" : self.cs_state_name, "cs_state_desc" : self.cs_state_desc}
