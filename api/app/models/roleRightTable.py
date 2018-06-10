3f700525698467cef29952b114948e6c2664c42b
Adam Kroon <adam.kroon@viryltech.com>
2018-06-09 22:49:49 -0700

    #5 Create models for all objects - roleRightTable model created

---
+++ api/app/models/roleRightTable.py
@@ -0,0 +1,22 @@
+from flask_restplus import fields
+from qsystem import api, db
+from base import Base 
+
+class RoleRightTable(MyBase, Base, db.model):
+
+    model = api.model('RoleRightTable' {
+        'role_id' : fields.Integer,
+        'right_id' : fields.Integer
+        })
+
+    role_id = db.Column(db.BigInteger)
+    right_id = db.Column(db.BigInteger)
+
+    # TODO do we need repr for testing?
+
+    def __init__(self, role_id, right_id):
+        self.role_id = role_id
+        self.right_id = right_id
+
+    def json (self, role_id, right_id):
+        return {"role_id" : self.role_id, "right_id" : self.right_id}
