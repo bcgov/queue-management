67e97df1357876d396f4cce2ae40ad90d2c9c4e3
Adam Kroon <adam.kroon@viryltech.com>
2018-06-11 22:02:00 -0700

    #5 Create models for all objects - class parameter cleanup, formatting, model cleanup

--- api/app/models/office_service.py
+++ api/app/models/office_service.py
@@ -2,19 +2,20 @@ from flask_restplus import fields
 from qsystem import api, db
 from base import Base 
 
-class OfficeServiceTable(MyBase, Base, db.model):
+class OfficeService(Base):
 
-    model = api.model('OfficeServiceTable' {
+    model = api.model('OfficeService' {
         'office_id' : fields.Integer,
         'service_id' : fields.Integer
         })
 
-    office_id = db.Column(db.BigInteger)
-    service_id = db.Column(db.BigInteger)
+    office_id   = db.Column(db.BigInteger)
+    service_id  = db.Column(db.BigInteger)
 
     def __init__(self, office_id, service_id):
-        self.office_id = office_id
+        self.office_id  = office_id
         self.service_id = service_id
 
     def json(self, office_id, service_id):
-        return {"office_id" : self.office_id, "service_id" : self.service_id}
+        return {"office_id" : self.office_id, 
+                "service_id" : self.service_id}
