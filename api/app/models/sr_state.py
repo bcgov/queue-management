b57e70fc0653bb9d63ebde265095e2fee10638eb
Adam Kroon <adam.kroon@viryltech.com>
2018-06-11 21:22:02 -0700

    #5 Create models for all objects - sr_state.py renamed class, cleaned up inheritance, formatting

--- api/app/models/sr_state.py
+++ api/app/models/sr_state.py
@@ -2,25 +2,27 @@ from flask_restplus import fields
 from qsystem import api, db
 from base import Base 
 
-class SRStateTable(MyBase, Base, db.model):
+class SRState(Base, db.model):
 
-    model = api.model('SRStateTable' {
+    model = api.model('SRState' {
         'sr_state_id' : fields.Integer,
         'sr_code' : fields.String,
         'sr_state_desc' fields,String
         })
 
-    sr_state_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    sr_code = db.Column(db.String(100))
-    sr_state_desc = db.Column(db.String(1000))
+    sr_state_id     = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    sr_code         = db.Column(db.String(100))
+    sr_state_desc   = db.Column(db.String(1000))
 
     def __repr__(self, sr_code):
         return '<SR Code: %r>' % self.sr_code   
 
     def __init__(self, sr_state_id, sr_code, sr_state_desc):
-        self.sr_state_id = sr_state_id
-        self.sr_code = sr_code
-        self.sr_state_desc  =  sr_state_desc
+        self.sr_state_id    = sr_state_id
+        self.sr_code        = sr_code
+        self.sr_state_desc  = sr_state_desc
 
     def json(self, sr_state_id, sr_code, sr_state_desc):
-        return {"sr_state_id" : self.sr_state_id, "sr_code" : self.sr_code, "sr_state_desc" : self.sr_state_desc}
+        return {"sr_state_id" : self.sr_state_id, 
+                "sr_code" : self.sr_code, 
+                "sr_state_desc" : self.sr_state_desc}
