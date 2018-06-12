a021ea9f16f61c91f8dd48e0ed270825f051a488
Adam Kroon <adam.kroon@viryltech.com>
2018-06-11 22:11:29 -0700

    #5 Create models for all objects - class parameter cleanup, formatting, model cleanup

--- api/app/models/role_right.py
+++ api/app/models/role_right.py
@@ -2,21 +2,22 @@ from flask_restplus import fields
 from qsystem import api, db
 from base import Base 
 
-class RoleRightTable(MyBase, Base, db.model):
+class RoleRight(Base):
 
-    model = api.model('RoleRightTable' {
+    model = api.model('RoleRight' {
         'role_id' : fields.Integer,
         'right_id' : fields.Integer
         })
 
-    role_id = db.Column(db.BigInteger)
-    right_id = db.Column(db.BigInteger)
+    role_id     = db.Column(db.BigInteger)
+    right_id    = db.Column(db.BigInteger)
 
     # TODO do we need repr for testing?
 
     def __init__(self, role_id, right_id):
-        self.role_id = role_id
-        self.right_id = right_id
+        self.role_id    = role_id
+        self.right_id   = right_id
 
     def json (self, role_id, right_id):
-        return {"role_id" : self.role_id, "right_id" : self.right_id}
+        return {"role_id" : self.role_id, 
+                "right_id" : self.right_id}
