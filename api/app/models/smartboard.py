2ac97fac04d6942ce8307627c38b75aa58792b10
Adam Kroon <adam.kroon@viryltech.com>
2018-06-11 22:18:11 -0700

    #5 Create models for all objects - class parameter cleanup, formatting, model cleanup

--- api/app/models/smartboard.py
+++ api/app/models/smartboard.py
@@ -2,23 +2,24 @@ from flask_restplus import fields
 from qsystem import api, db
 from base import Base 
 
-class SmartBoardTable(MyBase, Base, db.model):
+class SmartBoard(Base):
 
-    model = api.model ('SmartBoardTable' {
-        sb_id = fields.Integer,
+    model = api.model ('SmartBoard' {
+        sb_id : fields.Integer,
         # TODO is type a reserved word that we need to change the column name for?
-        type = fields.String
+        type : fields.String
         })
 
-    sb_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    type = db.Column(db.String(45))
+    sb_id   = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    type    = db.Column(db.String(45))
 
     def __repr__(self, type):
         return '<Smartboard Type: %r>' % self.type
 
     def __init__(self, sb_id, type):
-        self.sb_id = sb_id
-        self.type = type
+        self.sb_id  = sb_id
+        self.type   = type
 
     def json(self, sb_id, type):
-        return {"sb_id" : self.sb_id, "type" : self.type}
+        return {"sb_id" : self.sb_id, 
+                "type" : self.type}
