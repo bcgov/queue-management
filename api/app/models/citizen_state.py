f991d48dc3585be0efd9d7b91211ce9929e80feb
Adam Kroon <adam.kroon@viryltech.com>
2018-06-11 21:41:52 -0700

    #5 Create models for all objects - class parameter cleanup, formatting, model cleanup

--- api/app/models/citizen_state.py
+++ api/app/models/citizen_state.py
@@ -2,17 +2,17 @@ from flask_restplus import fields
 from qsystem import api, db
 from base import Base 
 
-class CitizenStateTable(MyBase, Base, db.model):
+class CitizenStateTable(Base):
 
     model = api.model('CitizenStateTable' {
-        'cs_id' = fields.Integer,
-        'cs_state_name' = fields.String,
-        'cs_state_desc' = fields.String
+        'cs_id' : fields.Integer,
+        'cs_state_name' : fields.String,
+        'cs_state_desc' : fields.String
         })
 
-    cs_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    cs_state_name = db.Column(db.String(100))
-    cs_state_desc = db.Column(db.String(1000))
+    cs_id           = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    cs_state_name   = db.Column(db.String(100))
+    cs_state_desc   = db.Column(db.String(1000))
 
     def __repr__(self, cs_state_name):
         return '<CS State Name: %r>' % self.cs_state_name
@@ -23,4 +23,6 @@ class CitizenStateTable(MyBase, Base, db.model):
         self.cs_state_desc = cs_state_desc
 
     def json(self, cs_id, cs_state_name, cs_state_desc)
-        return {"cs_id" : self.cs_id, "cs_state_name" : self.cs_state_name, "cs_state_desc" : self.cs_state_desc}
+        return {"cs_id" : self.cs_id, 
+                "cs_state_name" : self.cs_state_name, 
+                "cs_state_desc" : self.cs_state_desc}
