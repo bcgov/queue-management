5aad71750ff90da170e5fe2bbe1597dc8228c76d
Adam Kroon <adam.kroon@viryltech.com>
2018-06-11 22:16:39 -0700

    #5 Create models for all objects - class parameter cleanup, formatting, model cleanup

--- api/app/models/service_req.py
+++ api/app/models/service_req.py
@@ -2,9 +2,9 @@ from flask_restplus import fields
 from qsystem import api, db
 from base import Base 
 
-class ServiceReqTable(MyBase, Base, db.model):
+class ServiceReq(Base):
 
-    model = api.model('ServiceReqTable' {
+    model = api.model('ServiceReq' {
         'sr_id' : fields.Integer,
         'citizen_id' : fields.Integer,
         'quantity' : fields.Integer,
@@ -12,21 +12,24 @@ class ServiceReqTable(MyBase, Base, db.model):
         'sr_state_id' :  fields.Integer
         })
 
-    sr_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    citizen_id = db.Column(db.BigInteger)
-    quantity =  db.Column(db.Integer)
-    service_id = db.Column(db.BigInteger)
+    sr_id       = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    citizen_id  = db.Column(db.BigInteger)
+    quantity    =  db.Column(db.Integer)
+    service_id  = db.Column(db.BigInteger)
     sr_state_id = db.Column(db.BigInteger)
 
     # TODO is repr needed for testing?
 
     def __init__(self, sr_id, citizen_id, quantity, service_id, sr_state_id):
-        self.sr_id = sr_id
-        self.citizen_id = citizen_id
-        self.quantity = quantity
-        self.service_id = service_id
-        self.sr_state_id
+        self.sr_id          = sr_id
+        self.citizen_id     = citizen_id
+        self.quantity       = quantity
+        self.service_id     = service_id
+        self.sr_state_id    = sr_state_id
 
     def json(self, sr_id, citizen_id, quantity, service_id, sr_state_id):
-        return {"sr_id" : self.sr_id, "citizen_id" : self.citizen_id, "quantity" : self.quantity, 
-                "service_id" : self.service_id, "sr_state_id" : self.sr_state_id}
+        return {"sr_id" : self.sr_id, 
+                "citizen_id" : self.citizen_id, 
+                "quantity" : self.quantity, 
+                "service_id" : self.service_id, 
+                "sr_state_id" : self.sr_state_id}
