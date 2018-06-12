8b89e3b875a7b9545fb53f025463d4eb63cd86d7
Adam Kroon <adam.kroon@viryltech.com>
2018-06-11 22:33:54 -0700

    #5 Create models for all objects - added args, json_args dictionaries to be passed into init and json functions

--- api/app/models/csr.py
+++ api/app/models/csr.py
@@ -31,23 +31,28 @@ class CSR(Base):
     def __repr__(self, username):
         return '<CSR User: %r>' % self.username
 
-    # TODO redo init function to include kwargs
-    def __init__(self, csr_id, username, password, office_id_in_now, role_id, qt_xn_csr_now, 
-                receptionist_now, deleted, csr_state_id_now):
-        self.csr_id             = csr_id
-        self.username           = username
-        self.password           = password
-        self.office_id_in_now   = office_id_in_now
-        self.role_id            = role_id
-        self.qt_xn_csr_now      = qt_xn_csr_now
-        self.receptionist_now   = receptionist_now
-        self.deleted            = deleted
-        self.csr_state_id_now   = csr_state_id_now
+    args = {'csr_id': 'csr_id', 
+            'username': 'username', 
+            'password': 'password', 
+            'office_id_in_now': 'office_id_in_now', 
+            'role_id': 'role_id', 
+            'qt_xn_csr_now': 'qt_xn_csr_now', 
+            'receptionist_now': 'receptionist_now', 
+            'deleted': 'deleted', 
+            'csr_state_id_now': 'csr_state_id_now' }
 
-    # TODO Check normal line length for large return values 
-    def json(self, csr_id, username, password, office_id_in_now, role_id, qt_xn_csr_now, 
-            receptionist_now, deleted, csr_state_id_now):
-        return {"csr_id" : self.csr_id, 
+    def __init__(self, **args):
+        self.csr_id             = args['csr_id']
+        self.username           = args['username']
+        self.password           = args['password']
+        self.office_id_in_now   = args['office_id_in_now']
+        self.role_id            = args['role_id']
+        self.qt_xn_csr_now      = args['qt_xn_csr_now']
+        self.receptionist_now   = args['receptionist_now']
+        self.deleted            = args['deleted']
+        self.csr_state_id_now   = args['csr_state_id_now']
+
+    json_args = {"csr_id" : self.csr_id, 
                 "username" : self.username, 
                 "password" : self.password, 
                 "office_id_in_now" : office_id_in_now, 
@@ -56,3 +61,6 @@ class CSR(Base):
                 "receptionist_now" : self.receptionist_now, 
                 "deleted" : self.deleted, 
                 "csr_state_id_now" : self.csr_state_id_now}
+
+    def json(self, json_args):
+        return json_args
