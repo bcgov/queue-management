dffef2f6ae12ffe8d6c463869e1ccd4533c9375b
Adam Kroon <adam.kroon@viryltech.com>
2018-06-11 22:07:03 -0700

    #5 Create models for all objects - class parameter cleanup, formatting, model cleanup

--- api/app/models/period_state.py
+++ api/app/models/period_state.py
@@ -3,16 +3,16 @@ from qsystem import api, db
 from base import Base 
 
 # TODO Please review the valid state table to see for testing
-class PeriodStateTable(MyBase, Base, db.model):
+class PeriodState(Base):
 
-    model = api.model('PeriodStateTable' {
-        'ps_id' = fields.Integer,
-        'ps_name' = fields.String,
-        'ps_desc' = fields.String,
-        'ps_number' = fields.Integer
+    model = api.model('PeriodState' {
+        'ps_id' : fields.Integer,
+        'ps_name' : fields.String,
+        'ps_desc' : fields.String,
+        'ps_number' : fields.Integer
         })
 
-    ps_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    ps_id   = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     ps_name = db.Column(db.String(100))
     ps_desc = db.Column(db.String(1000))
     ps_name = db.Column(db.Integer)
@@ -21,10 +21,13 @@ class PeriodStateTable(MyBase, Base, db.model):
         return '<Period State Name: %r>' % self.ps_name
 
     def __init__(self, ps_id, ps_name, ps_desc, ps_number):
-        self.ps_id = ps_id
-        self.ps_name = ps_name
-        self.ps_desc = ps_desc
-        self.ps_number = ps_number
+        self.ps_id      = ps_id
+        self.ps_name    = ps_name
+        self.ps_desc    = ps_desc
+        self.ps_number  = ps_number
 
     def json(self, ps_id, ps_name, ps_desc, ps_number):
-        return {"ps_id" : self.ps_id, "ps_name" : self.ps_name, "ps_desc" : self.ps_desc, "ps_number" : self.ps_number}
+        return {"ps_id" : self.ps_id, 
+                "ps_name" : self.ps_name, 
+                "ps_desc" : self.ps_desc, 
+                "ps_number" : self.ps_number}
