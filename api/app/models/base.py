d493db42104716c592b07d2c905d0ac3a4290ddd
Adam Kroon <adam.kroon@viryltech.com>
2018-06-12 22:22:36 -0700

    #5 Create models for all objects - added __abstract__ = True to base class

--- api/app/models/base.py
+++ api/app/models/base.py
@@ -3,6 +3,7 @@ from qsystem import api, db
 from sqlalchemy.ext.declarative import declared_attr
 
 class Base(db.Model, object):
+    __abstract__ = True
 
     @declared_attr
     def __tablename__(cls):
