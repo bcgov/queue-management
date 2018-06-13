bc849320916b06da4ef24c1a5e5eeb62121fd0b8
Adam Kroon <adam.kroon@viryltech.com>
2018-06-12 18:14:04 -0700

    #5 Create models for all objects - changed base class name

--- api/app/models/base.py
+++ api/app/models/base.py
@@ -2,7 +2,7 @@ from flask_restplus import fields
 from qsystem import api, db
 from sqlalchemy.ext.declarative import declared_attr
 
-class MyBase(object, db.Model):
+class Base(object, db.Model):
 
     @declared_attr
     def __tablename__(cls):
