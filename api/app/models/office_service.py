a806e6cfb0657dc531b8a91e6b6d77f617e97915
Adam Kroon <adam.kroon@viryltech.com>
2018-06-13 19:31:29 -0700

    #5 Create models for all objects - many-to-many relationship for office_service

--- api/app/models/office_service.py
+++ api/app/models/office_service.py
@@ -3,6 +3,13 @@ from qsystem import api, db
 from .base import Base 
 from sqlalchemy import BigInteger
 
+office_service = db.Table('OfficeService',
+                 db.Column('office_id', db.BigInteger, 
+                            db.ForeignKey('office.office_id'), primary_key=True),
+                 db.Column('service_id', db.BigInteger,
+                            db.ForeignKey('service.service_id'), primary_key=True)
+)
+
 class OfficeService(Base):
 
     model = api.model('OfficeService', {
@@ -10,8 +17,8 @@ class OfficeService(Base):
         'service_id' : fields.Integer
         })
 
-    office_id   = db.Column(db.BigInteger)
-    service_id  = db.Column(db.BigInteger)
+    office_id  = db.Column(db.BigInteger, primary_key=True)
+    service_id = db.Column(db.BigInteger, primary_key=True)
 
     def __init__(self, office_id, service_id):
         self.office_id  = office_id
