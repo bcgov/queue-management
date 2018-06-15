919280c812d168e1ec7dfe92e723a29eee93031b
Adam Kroon <adam.kroon@viryltech.com>
2018-06-14 19:01:44 -0700

    Create models for all objects - added fetch, save functions to base class

--- api/app/models/base.py
+++ api/app/models/base.py
@@ -1,6 +1,8 @@
 from flask_restplus import fields
 from qsystem import api, db
 from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import sessionmaker
+from cockroachdb.sqlalchemy import run_transaction
 
 class Base(db.Model, object):
     __abstract__ = True
@@ -8,3 +10,13 @@ class Base(db.Model, object):
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
+
+    def save_to_db(self, session):
+        session.add(self)
+
+    def save(self):
+        sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
+        run_transaction(sessionmaker, self.save_to_db)
+
+    def get_by_id(self, id):
+        run_transaction(self.query.get(id))
