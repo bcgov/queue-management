ee1dd9cbf2fb3c9926cbdc44c53afac7c6072626
Adam Kroon <adam.kroon@viryltech.com>
2018-06-24 09:34:27 -0700

    Added BC Government Licenses to all files in models, schemas, resources.

--- api/app/resources/services.py
+++ api/app/resources/services.py
@@ -1,3 +1,17 @@
+'''Copyright 2018 Province of British Columbia
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.'''
+
 from flask import request, jsonify, g
 from flask_restplus import Resource
 import sqlalchemy.orm
@@ -37,7 +51,7 @@ class Services(Resource):
                 services = Service.query.all()
                 result = self.services_schema.dump(services)
                 return jsonify({'services': result})
-                
+
             except exc.SQLAlchemyError as e:
                 print (e)
                 return {"message": "api is down"}, 500
