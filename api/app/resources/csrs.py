c839aa8013144bea962da73b1aed56672362e7b3
Sean Rumsby <sean@olivewoodsoftware.com>
2018-08-14 12:03:29 -0700

    Add code to strip 'idir/' from the front of usernames

--- api/app/resources/csrs.py
+++ api/app/resources/csrs.py
@@ -28,7 +28,8 @@ class Services(Resource):
     @oidc.accept_token(require_token=True)
     def get(self):
         try:
-            csr = CSR.query.filter_by(username=g.oidc_token_info['username']).first()
+            username = g.oidc_token_info['username'].split("idir/")[-1]
+            csr = CSR.query.filter_by(username=username).first()
             result = self.csr_schema.dump(csr)
 
             return {'csr': result.data,
