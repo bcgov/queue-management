912bdba8f86001d4745aa913d0cce91d1b04c5ca
Sean Rumsby <sean@olivewoodsoftware.com>
2018-08-14 16:45:30 -0700

    Configure where we drop the keycloak config map

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -5,5 +5,5 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
   API_URL: '"/api/v1"',
   SOCKET_URL: '""',
-  KEYCLOAK_JSON_URL: '"/static/keycloak.json"'
+  KEYCLOAK_JSON_URL: '"/static/keycloak/keycloak.json"'
 })
