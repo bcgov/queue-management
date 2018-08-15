d45926c5bd27d6196ceda75fbf1e2afc11a220f6
Sean Rumsby <sean@olivewoodsoftware.com>
2018-08-14 17:13:45 -0700

    Added test and production configs

--- api/config.py
+++ api/config.py
@@ -2,7 +2,11 @@ import logging
 import os
 
 config = {
+    "production": "config.ProductionConfig",
+    "prod": "config.ProductionConfig",
+    "test": "config.TestConfig",
     "development": "config.DevelopmentConfig",
+    "dev": "config.DevelopmentConfig",
     "localhost": "config.LocalConfig",
     "default": "config.LocalConfig"
 }
@@ -16,7 +20,7 @@ class BaseConfig(object):
     LOGGING_LEVEL = DEBUG
     LOGGING_FORMAT = '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
 
-    SECRET_KEY = os.getenv('SECRET_KEY','d3a728fc-0671-4c08-82fe-602c695450cf')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     OIDC_OPENID_REALM = os.getenv('OIDC_OPENID_REALM','nest')
     OIDC_CLIENT_SECRETS = os.getenv('OIDC_SECRETS_FILE','client_secrets/secrets.json')
     OIDC_USER_INFO_ENABLED = True
@@ -29,7 +33,6 @@ class LocalConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     ACTIVE_MQ_URL = ''      #'amqp://guest:guest@localhost:5672'
     CORS_ALLOWED_ORIGINS = ["http://localhost:8080"]
-    SECRET_KEY = 'a9eec0e0-23b7-4788-9a92-318347b9a39f'
     USE_HTTPS = False
     SQLALCHEMY_ECHO = False
     SLACK_URL = os.getenv('SLACK_URL')
@@ -39,9 +42,98 @@ class DevelopmentConfig(BaseConfig):
     REDIS_DEBUG = True
     TESTING = False
     ENV = 'dev'
-    SECRET_KEY = 'a9eec0e0-23b7-4788-9a92-318347b9a39f'
 
-    SESSION_COOKIE_DOMAIN = '.pathfinder.gov.bc.ca'
+    SESSION_COOKIE_DOMAIN = 'dev-theq.pathfinder.gov.bc.ca'
+    REMEMBER_COOKIE_DURATION = 86400
+    USE_HTTPS = True
+
+    CORS_ALLOWED_ORIGINS = [""]
+
+    ACTIVE_MQ_USER = os.getenv('ACTIVE_MQ_USER', '')
+    ACTIVE_MQ_PASSWORD = os.getenv('ACTIVE_MQ_PASSWORD', '')
+    ACTIVE_MQ_HOST = os.getenv('ACTIVE_MQ_HOST', '')
+    ACTIVE_MQ_PORT = os.getenv('ACTIVE_MQ_PORT', '')
+    ACTIVE_MQ_URL = 'amqp://{amq_user}:{amq_password}@{amq_host}:{amq_port}'.format(
+        amq_user=ACTIVE_MQ_USER,
+        amq_password=ACTIVE_MQ_PASSWORD,
+        amq_host=ACTIVE_MQ_HOST,
+        amq_port=ACTIVE_MQ_PORT
+    )
+
+    DB_ENGINE = os.getenv('DATABASE_ENGINE', '')
+    DB_USER = os.getenv('DATABASE_USERNAME', '')
+    DB_PASSWORD = os.getenv('DATABASE_PASSWORD','')
+    DB_NAME = os.getenv('DATABASE_NAME','')
+    DB_HOST = os.getenv('DATABASE_HOST','')
+    DB_PORT = os.getenv('DATABASE_PORT','')
+    SQLALCHEMY_DATABASE_URI = '{engine}://{user}:{password}@{host}:{port}/{name}'.format(
+        engine=DB_ENGINE,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        name=DB_NAME,
+    )
+
+    SLACK_URL = os.getenv('SLACK_URL')
+
+    if os.getenv('SQLALCHEMY_ECHO', "False") == "True":
+        SQLALCHEMY_ECHO=True
+    else:
+        SQLALCHEMY_ECHO=False
+
+class TestConfig(BaseConfig):
+    DEBUG = True
+    REDIS_DEBUG = True
+    TESTING = False
+    ENV = 'test'
+
+    SESSION_COOKIE_DOMAIN = 'test-theq.pathfinder.gov.bc.ca'
+    REMEMBER_COOKIE_DURATION = 86400
+    USE_HTTPS = True
+
+    CORS_ALLOWED_ORIGINS = [""]
+
+    ACTIVE_MQ_USER = os.getenv('ACTIVE_MQ_USER', '')
+    ACTIVE_MQ_PASSWORD = os.getenv('ACTIVE_MQ_PASSWORD', '')
+    ACTIVE_MQ_HOST = os.getenv('ACTIVE_MQ_HOST', '')
+    ACTIVE_MQ_PORT = os.getenv('ACTIVE_MQ_PORT', '')
+    ACTIVE_MQ_URL = 'amqp://{amq_user}:{amq_password}@{amq_host}:{amq_port}'.format(
+        amq_user=ACTIVE_MQ_USER,
+        amq_password=ACTIVE_MQ_PASSWORD,
+        amq_host=ACTIVE_MQ_HOST,
+        amq_port=ACTIVE_MQ_PORT
+    )
+
+    DB_ENGINE = os.getenv('DATABASE_ENGINE', '')
+    DB_USER = os.getenv('DATABASE_USERNAME', '')
+    DB_PASSWORD = os.getenv('DATABASE_PASSWORD','')
+    DB_NAME = os.getenv('DATABASE_NAME','')
+    DB_HOST = os.getenv('DATABASE_HOST','')
+    DB_PORT = os.getenv('DATABASE_PORT','')
+    SQLALCHEMY_DATABASE_URI = '{engine}://{user}:{password}@{host}:{port}/{name}'.format(
+        engine=DB_ENGINE,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        name=DB_NAME,
+    )
+
+    SLACK_URL = os.getenv('SLACK_URL')
+
+    if os.getenv('SQLALCHEMY_ECHO', "False") == "True":
+        SQLALCHEMY_ECHO=True
+    else:
+        SQLALCHEMY_ECHO=False
+
+class ProductionConfig(BaseConfig):
+    DEBUG = True
+    REDIS_DEBUG = True
+    TESTING = False
+    ENV = 'production'
+
+    SESSION_COOKIE_DOMAIN = 'theq.pathfinder.gov.bc.ca'
     REMEMBER_COOKIE_DURATION = 86400
     USE_HTTPS = True
 
