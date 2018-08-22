4d645701cd908f0ffebe17c434575d75b5ce487e
Sean Rumsby <sean@olivewoodsoftware.com>
2018-08-21 19:09:59 -0700

    Update smartboard displays
    Updated the smartboard displays to correctly render for various office typeas. Added support to host the video from a URL within the frontend application, and updated the corresponding openshift template with the changes.

--- frontend/src/smartboard/index.vue
+++ frontend/src/smartboard/index.vue
@@ -14,9 +14,12 @@ limitations under the License.*/
 
 <template>
   <div class="main-container">
-    <CallByNumber v-if="officetype==='callbyticket'"></CallByNumber>
+    <div class="top-flex-div">
+      <div class="flex-title">{{ date }} {{ time }}</div>
+    </div>
+    <CallByTicket v-if="officetype==='callbyticket'"></CallByTicket>
     <CallByName v-else-if="officetype==='callbyname' || officetype==='reception'"></CallByName>
-    <NonReception v-else-if="officetype==='nonreception'"></NonReception>
+    <NonReception v-else-if="officetype==='nocallonsmartboard'"></NonReception>
     <div v-else>Please stand by</div>
     <BoardSocket></BoardSocket>
   </div>
@@ -24,18 +27,18 @@ limitations under the License.*/
 
 <script>
 import BoardSocket from './board-socket'
-import CallByNumber from './callbynumber'
-import CallByName from './callbyname'
-import NonReception from './nonreception'
+import CallByTicket from './call-by-ticket'
+import CallByName from './call-by-name'
+import NonReception from './non-reception'
 import axios from 'axios'
 
 const Axios = axios.create({
-        baseURL: process.env.API_URL,
-        withCredentials: true,
-        headers: {
-          'Accept': 'application/json'
-        }
-      })
+  baseURL: process.env.API_URL,
+  withCredentials: true,
+  headers: {
+    'Accept': 'application/json'
+  }
+})
 
 export default {
   name: 'Smartboard',
@@ -44,16 +47,23 @@ export default {
     this.initializeBoard()
   },
 
-  components: { CallByName, CallByNumber, BoardSocket, NonReception },
+  mounted() {
+    setInterval( () => { this.now() }, 3000)
+  },
+
+  components: { CallByName, CallByTicket, BoardSocket, NonReception },
 
   data() {
     return {
-      officetype: ''
+      officetype: '',
+      options: {weekday:'long',year:'numeric',month:'long',day:'numeric'},
+      timeOpts: {hour12: true, hour: 'numeric', minute: '2-digit'},
+      time: ''
     }
   },
 
   computed: {
-    office_id() {
+    office_number() {
       let path = window.location.pathname.split('/')
       if (path.length >= 3) {
         return path[2]
@@ -62,41 +72,42 @@ export default {
       }
     },
     url() {
-      return `/smartboard/?office_number=${this.office_id}`
+      return `/smartboard/?office_number=${this.office_number}`
+    },
+    date() {
+      let d = new Date()
+      return d.toLocaleDateString('en-CA', this.options)
     }
   },
 
   methods: {
     initializeBoard() {
+      this.now()
       Axios.get(this.url).then( resp => {
         this.officetype = resp.data.office_type
       })
+    },
+    now() {
+      let d = new Date()
+      this.time = d.toLocaleTimeString('en-CA', this.timeOpts)
     }
   }
 }
 </script>
 
 <style>
-  .board-footer-div { display: flex; position: fixed; bottom: 0; left: 0; margins: 0px; height:13%; width: 100%;
-    justify-content: flex-start; align-items: center; font-size:2.2rem; }
-  .board-video-div { left: 0px; top: 0px; height: 504px; width: 896px; background-color: white; box-shadow: 2px 3px 10px rgba(0, 0, 0, .5); }
-  .board-video-div-2 { left: 0px; top: 0px; height: 605px; width: 1075px; background-color: white; box-shadow: 2px 3px
-  10px rgba(0, 0, 0, .5); }
-  .board-75-video { width: 75%; height: 100%; }
-  .main-container { position: fixed; top: 0; left: 0; height:100%; width: 100%; margin: 0px; background:linear-gradient(lemonchiffon,lightskyblue) }
-  .top-flex-div { display: flex; height:12%; justify-content: center; align-items: center; width: 100% }
-  .flex-title { font-size: 3.6rem; color: darkblue;
-    text-shadow: -1px 0 steelblue, 0 1px steelblue, 1px 0 steelblue, 0 -1px steelblue }
+  .main-container { position: fixed; top: 0; left: 0; height:100%; width: 100%; margin: 0px; }
+  .top-flex-div { display: flex; height:11%; justify-content: center; align-items: center; width: 100% }
+  .bottom-flex-div { display: flex; height:11%; justify-content: center; align-items: center; width: 100%; padding-top: 2%}
+  .flex-title { font-size: 7.2rem; color: darkblue; text-shadow: -1px 0 steelblue, 0 1px steelblue, 1px 0 steelblue, 0 -1px steelblue }
   .lg-boardtable-head { font-size: 2.3rem; text-align: center; height: 30px }
   .sm-boardtable-body { font-size: 1.8rem; text-align: center }
   .sm-boardtable-head { font-size: 1.8rem; text-align: center }
   .lg-boardtable-body { font-size: 2.5rem; text-align: left }
   .flashing-ticket {color: red; font-size: 1rem }
-  .board-video-div { left: 0px; top: 0px; height: 504px; width: 896px; background-color: white; box-shadow: 2px 3px 10px rgba(0, 0, 0, .5); }
   .board-content-div { background-color: white; box-shadow: 2px 3px 10px rgba(0, 0, 0, .5); }
   .board-table-style { width: 100%; background-color: white; text-align: center; }
-  .board-75-video { width: 75%; height: 100%; padding-left: 3%}
-  .board-25-table { width: 25%; height: 100%; padding-right: 3%;}
-  .testclass {height: 20px;}
-  .flex-title-2 { font-size: 2.0rem; color: midnightblue}
+  .board-85-video { width: 82%; max-height: 60vh; padding-left: 1%; padding-right: 1%;}
+  .board-25-table { width: 25%; max-height: 60vh; padding-left: 1%; padding-right: 1%;}
+  .flex-title { font-size: 4.0rem; color: midnightblue }
 </style>
