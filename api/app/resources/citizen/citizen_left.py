608e7db0500f33cbd6c365e447384feb20d4722d
Sean Rumsby <sean@olivewoodsoftware.com>
2018-08-26 15:40:00 -0700

    Smartboard bugs
    Fixed issue incorrectly processing back office tickets
    Fixed issues with not adding / removing tickets from now calling and waiting list as a citizen moves through the workflow

--- api/app/resources/citizen/citizen_left.py
+++ api/app/resources/citizen/citizen_left.py
@@ -50,6 +50,7 @@ class CitizenLeft(Resource):
         db.session.commit()
 
         socketio.emit('update_customer_list', {}, room=csr.office_id)
+        socketio.emit('citizen_invited', {}, room='sb-%s' % csr.office.office_number)
         result = self.citizen_schema.dump(citizen)
         socketio.emit('update_active_citizen', result.data, room=csr.office_id)
 
