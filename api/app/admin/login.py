b11ba81087c39fad47a13e0792f4a434702c30c9
Sean Rumsby <sean@olivewoodsoftware.com>
2018-09-22 13:20:35 -0700

    Removed login link from admin panels

--- api/app/admin/login.py
+++
@@ -1,8 +0,0 @@
-from flask_admin.menu import MenuLink
-from flask_login import current_user
-
-
-class LoginMenuLink(MenuLink):
-
-    def is_accessible(self):
-        return not current_user.is_authenticated
