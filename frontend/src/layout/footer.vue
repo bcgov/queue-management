4b83dae2d03c2de0741a191e7a8e4fe14ec1ac11
ChrisDMac <33269946+ChrisDMac@users.noreply.github.com>
2018-12-11 17:39:49 -0800

    Reset user cache (#144)
    * Refactor all cacheing to be in the theq/csr.py class

--- frontend/src/layout/footer.vue
+++ frontend/src/layout/footer.vue
@@ -23,7 +23,7 @@
         <a href="#" @click="keycloakLogin()" id="keycloak-login">Keycloak Login</a>
       </div>
       <div class="footer-anchor-item-last" style="display:inline-block; color: white; margin-right:15px;">
-        v1.0.12
+        v1.0.13
       </div>
     </div>
   </div>
