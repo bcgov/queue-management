db38365515caa1c4c82f741ae5f7943575078d87
Chris <Chris.McIntosh@gov.bc.ca>
2019-01-16 16:39:58 -0800

    Update the version

--- frontend/src/layout/footer.vue
+++ frontend/src/layout/footer.vue
@@ -23,7 +23,7 @@
         <a href="#" @click="keycloakLogin()" id="keycloak-login">Keycloak Login</a>
       </div>
       <div class="footer-anchor-item-last" style="display:inline-block; color: white; margin-right:15px;">
-        v1.0.14
+        v1.0.15
       </div>
     </div>
   </div>
