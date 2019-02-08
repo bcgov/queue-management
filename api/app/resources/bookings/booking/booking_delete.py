c1700463c348423f8bb01e50c01679be79b0383d
Sean Rumsby <sean@olivewoodsoftware.com>
2019-02-08 12:14:37 -0800

    Fixed bug prevent LIAISON from entering exams into a different office

--- api/app/resources/bookings/booking/booking_delete.py
+++ api/app/resources/bookings/booking/booking_delete.py
@@ -33,7 +33,7 @@ class BookingDelete(Resource):
         booking = Booking.query.filter_by(booking_id=id).first_or_404()
 
         # Also 404 the request if they shouldn't be able to see this booking
-        if booking.office_id != csr.office_id and csr.role_code != "LIAISON":
+        if booking.office_id != csr.office_id and csr.role.role_code != "LIAISON":
             abort(404)
 
         db.session.delete(booking)
