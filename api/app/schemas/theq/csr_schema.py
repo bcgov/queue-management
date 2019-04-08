09eb561318d448ca7c0bdbf6de3fcb6414a5b8c9
Clark Van Oyen <clark@countable.ca>
2019-04-07 19:14:21 -0700

    data migration

--- api/app/schemas/theq/csr_schema.py
+++ api/app/schemas/theq/csr_schema.py
@@ -30,7 +30,8 @@ class CSRSchema(ma.ModelSchema):
     username = fields.Str()
     office_id = fields.Int()
     role_id = fields.Int()
-    qt_xn_csr_ind = fields.Int()
+    # deprecated
+    #qt_xn_csr_ind = fields.Int()
     receptionist_ind = fields.Int()
     deleted = fields.DateTime()
     csr_state_id = fields.Int()
