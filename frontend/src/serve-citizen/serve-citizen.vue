cc63e296b45c7363d50b08b69c660926ec624209
Clark Van Oyen <clark@countable.ca>
2019-04-14 10:41:49 -0700

    Non-reception mode shoudn't show counters on serve modal.

--- frontend/src/serve-citizen/serve-citizen.vue
+++ frontend/src/serve-citizen/serve-citizen.vue
@@ -74,7 +74,7 @@
         <b-row no-gutters>
           <b-col cols="7" />
           <b-col cols="auto" style="align: right">
-            <select id="counter-selection" v-if="reception && !simplified" class="custom-select" v-model="counter_selection">
+            <select id="counter-selection" v-show="reception && !simplified" class="custom-select" v-model="counter_selection">
               <option v-for="counter in user.office.counters"
                     :value="counter.counter_id"
                     :key="counter.counter_id">
