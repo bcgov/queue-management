5c1af8812aab47ecae6bbcaa11ffe84e357398b8
Scott Rumsby <scott@olivewood.io>
2019-04-17 10:43:42 -0700

    Fixed the issue causing the @ not to load in IE at all
    Elimanated the legacy Babel 6-style configuration files for Babel (frontend/.babelrc) in favour of a Babel 7 style babel.config.js file.  Appropriately specified targets and environments necessary to support IE11 and Firefox/Chrome in all the required NODE_ENVs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.*/
 
 import Vue from 'vue'
-import "@babel/polyfill"
 import 'es6-promise/auto'
 import { store } from './store/'
 import BootstrapVue from 'bootstrap-vue'
