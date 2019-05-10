82d203139f600366e505ab808d7fdbcf597f48c4
Clark Van Oyen <clark@countable.ca>
2019-05-10 00:14:58 -0700

    Missing token decorator

--- api/app/resources/theq/services.py
+++ api/app/resources/theq/services.py
@@ -32,10 +32,11 @@ class Refresh(Resource):
     Refresh the quick lists to the 5 most frequently used items.
     Returns the resulting office object with updated lists indicated.
     """
+
+    @oidc.accept_token(require_token=True)
     def get(self):
         if request.args.get('office_id'):
             office_id = int(request.args.get('office_id'))
-
             csr = CSR.find_by_username(g.oidc_token_info['username'])
             
             if csr.role.role_code == "GA":
