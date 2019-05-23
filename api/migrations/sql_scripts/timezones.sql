9e1376ba49505f31423583d2d95b139ecbdc591f
Chris <Chris.McIntosh@gov.bc.ca>
2019-05-23 16:45:34 -0700

    Converting from MySQL to Postgres

--- api/migrations/sql_scripts/timezones.sql
+++
@@ -1,7 +0,0 @@
-use qsystem;
-
-insert into timezone (timezone_name) values ('America/Vancouver')
-
-insert into timezone (timezone_name) values ('America/Dawson_Creek')
-
-insert into timezone (timezone_name) values ('America/Edmonton')
