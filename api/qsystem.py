244dbdaf7114e0952abf410dfc82cfa4ad6b2f01
Chris <Chris.McIntosh@gov.bc.ca>
2019-06-05 16:01:26 -0700

    Put time stamps in error messages.

--- api/qsystem.py
+++ api/qsystem.py
@@ -23,6 +23,8 @@ from app.exceptions import AuthError
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
 
+import sys
+
 # from pprint import pprint
 
 application = Flask(__name__, instance_relative_config=True)
@@ -41,10 +43,37 @@ db = SQLAlchemy(application, engine_options={
 # db = SQLAlchemy(application)
 db.init_app(application)
 
-logging.basicConfig(level=logging.DEBUG)
+# def setup_custom_logger(name):
+#     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+#                                   datefmt='%Y-%m-%d %H:%M:%S')
+#     handler = logging.FileHandler('log.txt', mode='w')
+#     handler.setFormatter(formatter)
+#     screen_handler = logging.StreamHandler(stream=sys.stdout)
+#     screen_handler.setFormatter(formatter)
+#     logger = logging.getLogger(name)
+#     logger.setLevel(logging.DEBUG)
+#     logger.addHandler(handler)
+#     logger.addHandler(screen_handler)
+#     return logger
+
+# formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+#                               datefmt='%Y-%m-%d %H:%M:%S')
+# screen_handler = logging.StreamHandler(stream=sys.stdout)
+# screen_handler.setFormatter(formatter)
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(name)s %(message)s',
+                    level=logging.DEBUG,
+                    datefmt='%Y-%m-%d %H:%M:%S')
+# logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('sqlalchemy.engine').setLevel(logging.DEBUG)
+# logging.getLogger('sqlalchemy.engine').addHandler(screen_handler)
 logging.getLogger('sqlalchemy.pool').setLevel(logging.DEBUG)
+# logging.getLogger('sqlalchemy.pool').addHandler(screen_handler)
 logging.getLogger('psycopg2').setLevel(logging.DEBUG)
+# logging.getLogger('psycopg2').addHandler(screen_handler)
+
+logging.getLogger('sqlalchemy.engine').info(">>> This is some info")
+logging.getLogger('sqlalchemy.pool').error(">>> This is an error")
 
 print("==> DB engine settings:")
 print("    --> pool_size = {}".format(db.engine.pool.size()))
