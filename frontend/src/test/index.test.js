c7ae68f70df6465455ab14c827d6a818d1459b04
Chris <Chris.McIntosh@gov.bc.ca>
2019-07-31 18:08:05 -0700

    Updates for JEST test config.

--- frontend/src/test/index.test.js
+++ frontend/src/test/index.test.js
@@ -5,11 +5,17 @@ let browser;
 const width = 1200;
 const height = 800;
 const maxTestCaseTime = 30000;
+const USER_DATA_DIR = 'c:\\temp\\jest';
+const USER_DATA_DIR_WSL = '/mnt/c/temp/jest';
+
+
 
 beforeAll(async () => {
     browser = await puppeteer.launch({
+        executablePath: 'chrome.exe',
+        userDataDir: USER_DATA_DIR,
         headless: false,
-        slowMo: 100,
+        slowMo: 30,
         args: [`--window-size=${width},${height}`]
     });
     page = await browser.newPage();
