8a58cf09272f7df10bd8a8ea6bf38c1088eadf16
Chris <Chris.McIntosh@gov.bc.ca>
2019-09-16 16:55:56 -0700

    Add code to read and return correct video URL for the office

--- api/app/resources/theq/videofiles.py
+++ api/app/resources/theq/videofiles.py
@@ -14,10 +14,51 @@ limitations under the License.'''
 
 from flask_restplus import Resource
 from qsystem import api, oidc, application
+from app.models.theq import CSR
+from flask import g
 import os
 from os.path import isfile
 from pprint import pprint
 from datetime import datetime
+
+def ReadFile(entry):
+    with open(entry, "r") as myfile:
+        manifest_data = myfile.read()
+        # print("==> Manifest data")
+        # print(">>" + manifest_data + "<<")
+        return manifest_data
+
+def GetUrl(office_number, manifest_data):
+
+    error = "Neither office " + str(office_number) + ' "default" found in manifest.json'
+
+    #  Search for office number, if not found, search for defauult.
+    search = '"' + str(office_number) + '"'
+    index = manifest_data.find(search)
+    if (index < 0):
+        search = '"default"'
+        index = manifest_data.find(search)
+
+    #  If neither office or default found, an error in the manifest.
+    if (index < 0):
+        return { 'videourl' : '',
+                 'errors' : error}
+
+    print("==> In GetURL: Office: " + str(office_number) + "; Search: " + search + "; Index: " + str(index))
+
+    #  Found the right office.  Now look for it's URL.
+    left = manifest_data[index:]
+    index = left.find('"url"')
+    left = left[index+6:]
+    index = left.find('"')
+    left = left[index+1:]
+    index = left.find('"')
+    url = left[:index]
+    print("    --> URL is: " + url)
+
+    return {'videourl': url,
+            'errors': '',
+            'test': 'Hi there'}
 
 @api.route("/videofiles/", methods=["GET"])
 class VideoFiles(Resource):
@@ -42,12 +83,43 @@ class VideoFiles(Resource):
                         newfiles.append(new_info)
 
                     if entry.name.lower() == 'manifest.json':
-                        with open (entry, "r") as myfile:
-                            # manifest_data = myfile.readlines()
-                            manifest_data = myfile.read()
-                            print("==> Manifest data")
-                            print(">>" + manifest_data + "<<")
+                        manifest_data = ReadFile(entry)
+                        # ToDo xxx PUT IN ERROR CHECKING!!!
+                        # with open (entry, "r") as myfile:
+                        #     manifest_data = myfile.read()
+                        #     print("==> Manifest data")
+                        #     print(">>" + manifest_data + "<<")
 
         return {'videofiles': newfiles,
                 'manifest' : manifest_data,
                 'errors': ''}
+
+@api.route("/videofiles/<int:office_number>", methods=["GET"])
+class VideoFileSelf(Resource):
+
+    # @oidc.accept_token(require_token=True)
+    def get(self, office_number):
+
+        # try:
+
+            office = office_number
+            manifest_data = ""
+
+            print("==> In GET /videofiles/me/: Office number is: " + str(office))
+
+            video_path = application.config['VIDEO_PATH']
+            with os.scandir(video_path) as dir_entries:
+                for entry in dir_entries:
+                    if isfile(entry):
+                        if entry.name.lower() == 'manifest.json':
+                            manifest_data = ReadFile(entry)
+
+            print("    --> Manifest data is:")
+            print(manifest_data)
+
+            result = GetUrl(office, manifest_data)
+
+            return result
+
+    # except:
+        #     print("==> Error in VideoFileSelf")
