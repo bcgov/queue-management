202c81a85bb70575c7f493b40194fb82dd03a717
Chris <Chris.McIntosh@gov.bc.ca>
2019-11-14 17:09:37 -0800

    Fix bug in setting error level Use the print_flag when printing logging levels

--- api/config.py
+++ api/config.py
@@ -198,8 +198,9 @@ def configure_logging(app):
             module_logger = logging.getLogger(name)
             log_level = debug_string_to_debug_level(log_string)
             module_logger.setLevel(log_level)
-        if (basic_string != "DEFAULT"):
-            print("        --> Logger " + name + " set to level LOG_BASIC level of " + basic_string)
+        elif (basic_string != "DEFAULT"):
+            if print_flag:
+                print("        --> Logger " + name + " set to level LOG_BASIC level of " + basic_string)
             module_logger = logging.getLogger(name)
             module_logger.setLevel(basic_level)
 
