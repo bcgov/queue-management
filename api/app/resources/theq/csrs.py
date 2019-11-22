9f8aa2d7fae8f6c31ec2836ea7dfaf690bf5502a
Chris <Chris.McIntosh@gov.bc.ca>
2019-11-21 16:00:13 -0800

    Code changes to the back end for new Exam Manager Action Items functionality

--- api/app/resources/theq/csrs.py
+++ api/app/resources/theq/csrs.py
@@ -23,7 +23,6 @@ from app.schemas.bookings import ExamSchema, ExamTypeSchema
 from app.schemas.theq import CitizenSchema, CSRSchema
 import pytz
 
-
 @api.route("/csrs/", methods=["GET"])
 class CsrList(Resource):
 
@@ -81,88 +80,66 @@ class CsrSelf(Resource):
                 .filter_by(csr_id=csr.csr_id) \
                 .filter(Period.time_end.is_(None))
 
-            print("==> Exam attention required")
-
-            #   New business requirement, individual exam needs attention.
-            individual_attention = Exam.query \
+            #   Get a list of all current exams for the office.
+            office_exams = Exam.query \
                 .filter(Exam.office_id == csr.office_id, \
                         Exam.exam_returned_date.is_(None), \
                         Exam.deleted_date.is_(None)) \
                 .join(ExamType, Exam.exam_type_id == ExamType.exam_type_id) \
-                .filter(ExamType.group_exam_ind == 0) \
-                .join(Booking, Exam.booking_id == Booking.booking_id) \
-                .filter(or_(Booking.end_time < start_date, Exam.expiry_date <= today)).count()
+                .outerjoin(Booking, Exam.booking_id == Booking.booking_id) \
+                .outerjoin(Booking.booking_invigilators, Booking.booking_id == Booking.booking_invigilators.c.invigilator_id)
 
-            print("    --> Individual attention required count: " + str(individual_attention))
+            #   Default condition ... attention is not needed for any exam.
+            attention_needed = False
 
-            # New business requirement, monthly session exam needs attention.
-            session_attention = Exam.query \
-                .filter_by(office_id = csr.office_id) \
-                .join(ExamType, Exam.exam_type_id == ExamType.exam_type_id) \
-                .filter(ExamType.exam_type_name == 'Monthly Session Exam') \
-                .join(Booking, Exam.booking_id == Booking.booking_id) \
-                .outerjoin(Booking.booking_invigilators) \
-                .count()
-                # .filter(Booking.booking_invigilators.c.invigilator_id.is_(None)) \
-                # .first()
-                # .outerjoin(Booking.booking_invigilators, Booking.booking_id == Booking.booking_invigilators.c.invigilator_id) \
-                # .filter(Booking.end_time < start_date) \
-                # .join(Booking.booking_invigilators, Booking.booking_id == Booking.booking_invigilators.c.invigilator_id) \
-                # .filter(Booking.booking_invigilators.c.invigilator_id.isnot(None)) \
-                # .count()
+            #   Check for attention needed, individual exam.
+            individual = []
+            for exam in office_exams:
+                if exam.exam_type.group_exam_ind == 0 and exam.exam_type.exam_type_name != 'Monthly Session Exam':
+                    attention_needed = attention_needed or exam.expiry_date <= start_date
+                    if exam.booking is not None:
+                        if exam.booking.end_time < start_date:
+                            attention_needed = True
+                    if attention_needed:
+                        individual.append(exam)
 
-            print("    --> Session attention required count: " + str(session_attention))
-            # print("==> Session attention")
-            # print(session_attention)
-            # for attr in dir(session_attention.booking.booking_invigilators):
-            #     print("    --> session_attention.booking.booking_invigilators." + attr + " = " + str(getattr(session_attention.booking.booking_invigilators, attr)))
+            #   Check for attention needed, monthly session exam, only if attention not already needed.
 
-            # print("    --> Monthly Session attention required count: " + str(session_attention))
+            # if not attention_needed:
+            #   TAKE OUT!!!!!
+            attention_needed = False
+            monthly = []
+            if not attention_needed:
+                for exam in office_exams:
+                    if exam.exam_type.exam_type_name == 'Monthly Session Exam':
+                        if exam.booking is None:
+                            attention_needed = True
+                        else:
+                            attention_needed = attention_needed or len(exam.booking.invigilators) == 0
+                            attention_needed = attention_needed or exam.booking.end_time < start_date
+                        if attention_needed:
+                            monthly.append(exam)
 
-            individual_exams = Exam.query \
-                .filter_by(office_id=csr.office_id) \
-                .filter(Exam.exam_returned_date.is_(None),
-                        Exam.expiry_date <= today,
-                        Exam.deleted_date.is_(None)) \
-                .join(ExamType, Exam.exam_type_id == ExamType.exam_type_id) \
-                .filter(ExamType.group_exam_ind == 0).count()
-
-            individual_exams_past_schedule = Exam.query \
-                .join(Booking, Exam.booking_id == Booking.booking_id)\
-                .filter(Booking.start_time < start_date)\
-                .filter_by(office_id=csr.office_id) \
-                .join(ExamType, Exam.exam_type_id == ExamType.exam_type_id) \
-                .filter(ExamType.group_exam_ind == 0).count()
-
-            group_exams = Exam.query \
-                .filter_by(office_id=csr.office_id) \
-                .filter(Exam.deleted_date.is_(None)) \
-                .join(ExamType, Exam.exam_type_id == ExamType.exam_type_id) \
-                .filter(ExamType.group_exam_ind == 1) \
-                .join(Booking, Exam.booking_id == Booking.booking_id) \
-                .filter(Booking.sbc_staff_invigilated == 0).count()
-                #.filter(Booking.invigilator_id.is_(None))\ TODO: Update this plz
-
-            group_attention = Exam.query \
-                .filter_by(office_id=csr.office_id)\
-                .filter(Exam.deleted_date.is_(None))\
-                .filter(Exam.exam_returned_date.is_(None))\
-                .join(ExamType, Exam.exam_type_id == ExamType.exam_type_id)\
-                .filter(ExamType.group_exam_ind == 1)\
-                .join(Booking, Exam.booking_id == Booking.booking_id)\
-                .filter(Booking.start_time < start_date).count()
-
-            if group_attention > 0 and individual_exams > 0:
-                group_attention += individual_exams
+            # if not attention_needed:
+            #   TAKE OUT!!!!!
+            attention_needed = False
+            group = []
+            if not attention_needed:
+                for exam in office_exams:
+                    if exam.exam_type.group_exam_ind == 1:
+                        if exam.booking is None:
+                            attention_needed = True
+                        else:
+                            attention_needed = attention_needed or len(exam.booking.invigilators) == 0
+                            attention_needed = attention_needed or exam.booking.end_time < start_date
+                        if attention_needed:
+                            group.append(exam)
 
             result = self.csr_schema.dump(csr)
             active_citizens = self.citizen_schema.dump(active_citizens)
 
             return {'csr': result.data,
-                    'individual_exams': individual_exams,
-                    'individual_exams_past_schedule': individual_exams_past_schedule,
-                    'group_exams': group_exams,
-                    'group_individual_attention': group_attention,
+                    'attention_needed': attention_needed,
                     'active_citizens': active_citizens.data,
                     'back_office_display': self.back_office_display,
                     'recurring_feature_flag': self.recurring_feature_flag,
