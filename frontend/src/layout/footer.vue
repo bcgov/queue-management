4bc4807b31284d6348f24fb835844995e53130fc
Chris <Chris.McIntosh@gov.bc.ca>
2019-11-28 17:23:30 -0800

    Put in some debugging code Change the version

--- frontend/src/layout/footer.vue
+++ frontend/src/layout/footer.vue
@@ -24,7 +24,7 @@
           <a href="#" @click="keycloakLogin()" id="keycloak-login">Keycloak Login</a>
         </div>
         <div class="footer-anchor-item-last" style="display:inline-block; color: white; margin-right:15px;">
-          v1.0.68
+          v1.0.69
         </div>
       </div>
     </div>
