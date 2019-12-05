03d7ef390f7ac92ca4d8dbd45dae052d6788f1ad
Chris <Chris.McIntosh@gov.bc.ca>
2019-12-05 09:47:53 -0800

    Temporarily hide old Serve Now buttons.

--- frontend/src/add-citizen/form-components/buttons.vue
+++ frontend/src/add-citizen/form-components/buttons.vue
@@ -3,6 +3,7 @@
     <template v-if="$route.path === '/appointments' ">
       <div class="button-row">
         <b-button class="btn-success ml-3"
+                  v-if="1==2"
                   @click="addService">Add</b-button>
         <b-button class="btn-danger"
                   @click="closeAddServiceModal">Cancel</b-button>
@@ -43,6 +44,7 @@
             <b-button @click="beginService"
                       :disabled="performingAction || commentsTooLong"
                       class="btn-success"
+                      v-if="1==2"
                       id="add-citizen-begin-service">Begin service</b-button>
           </div>
         </div>
@@ -54,6 +56,7 @@
           <b-button @click="beginService"
                     :disabled="performingAction"
                     class="btn-success"
+                    v-if="1==2"
                     id="add-citizen-begin-service">Begin service</b-button>
         </div>
         <div v-if="simplified" class="buttons-div">
@@ -64,6 +67,7 @@
           <b-button @click="beginServiceSimplified"
                     :disabled="performingAction"
                     class="btn-success"
+                    v-if="1==2"
                     id="add-citizen-begin-service">Begin service</b-button>
         </div>
       </template>
