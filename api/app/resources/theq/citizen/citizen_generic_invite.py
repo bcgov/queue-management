5c98b4e8057684b5416ceeae1d7dbf421c885c66
Chris McIntosh <Chris.McIntosh@gov.bc.ca>
2020-03-09 11:02:36 -0700

    Test generic invite, minimal code

--- api/app/resources/theq/citizen/citizen_generic_invite.py
+++ api/app/resources/theq/citizen/citizen_generic_invite.py
@@ -19,7 +19,7 @@ from qsystem import api, api_call_with_retry, db, oidc, socketio, my_print
 from app.models.theq import Citizen, CSR, CitizenState, Period, PeriodState, ServiceReq, SRState
 from app.schemas.theq import CitizenSchema
 
-@api.route("/citizens/invite/", methods=['POST'])
+@api.route("/citizens/invitetest/", methods=['POST'])
 class CitizenGenericInvite(Resource):
 
     citizen_schema = CitizenSchema()
@@ -29,114 +29,5 @@ class CitizenGenericInvite(Resource):
     @api_call_with_retry
     def post(self):
 
-        line = 0
-        print("==> In Python /citizens/invite")
-        line = line + 1
-        print("    --> Line: " + str(line))
-        csr = CSR.find_by_username(g.oidc_token_info['username'])
-        line = line + 1
-        print("    --> Line: " + str(line))
-        lock = FileLock("lock/invite_citizen_{}.lock".format(csr.office_id))
-        line = line + 1
-        print("    --> Line: " + str(line))
-
-        with lock:
-
-            line = line + 1
-            print("    --> Line: " + str(line))
-            active_citizen_state = CitizenState.query.filter_by(cs_state_name='Active').first()
-            line = line + 1
-            print("    --> Line: " + str(line))
-            waiting_period_state = PeriodState.get_state_by_name("Waiting")
-            line = line + 1
-            print("    --> Line: " + str(line))
-            citizen = None
-            line = line + 1
-            print("    --> Line: " + str(line))
-            json_data = request.get_json()
-            line = line + 1
-            print("    --> Line: " + str(line))
-
-            if json_data and 'counter_id' in json_data:
-                counter_id = int(json_data.get('counter_id'))
-            else:
-                counter_id = int(csr.counter_id)
-
-            line = line + 1
-            print("    --> Line: " + str(line))
-            citizen = Citizen.query \
-                .filter_by(counter_id=counter_id, cs_id=active_citizen_state.cs_id, office_id=csr.office_id) \
-                .join(Citizen.service_reqs) \
-                .join(ServiceReq.periods) \
-                .filter_by(ps_id=waiting_period_state.ps_id) \
-                .filter(Period.time_end.is_(None)) \
-                .order_by(Citizen.priority, Citizen.citizen_id) \
-                .first()
-
-            # If no matching citizen with the same counter type, get next one
-            line = line + 1
-            print("    --> Line: " + str(line))
-            if citizen is None:
-                citizen = Citizen.query \
-                    .filter_by(cs_id=active_citizen_state.cs_id, office_id=csr.office_id) \
-                    .join(Citizen.service_reqs) \
-                    .join(ServiceReq.periods) \
-                    .filter_by(ps_id=waiting_period_state.ps_id) \
-                    .filter(Period.time_end.is_(None)) \
-                    .order_by(Citizen.priority, Citizen.citizen_id) \
-                    .first()
-
-            line = line + 1
-            print("    --> Line: " + str(line))
-            if citizen is None:
-                return {"message": "There is no citizen to invite"}, 400
-
-            line = line + 1
-            print("    --> Line: " + str(line))
-            my_print("==> POST /citizens/invite/ Citizen: " + str(citizen.citizen_id) + ', Ticket: ' + citizen.ticket_number)
-
-            line = line + 1
-            print("    --> Line: " + str(line))
-            db.session.refresh(citizen)
-            line = line + 1
-            print("    --> Line: " + str(line))
-            active_service_request = citizen.get_active_service_request()
-
-            line = line + 1
-            print("    --> Line: " + str(line))
-            try:
-                active_service_request.invite(csr, invite_type="generic", sr_count = len(citizen.service_reqs))
-            except TypeError:
-                return {"message": "Error inviting citizen. Please try again."}, 400
-
-            line = line + 1
-            print("    --> Line: " + str(line))
-            active_service_state = SRState.get_state_by_name("Active")
-            line = line + 1
-            print("    --> Line: " + str(line))
-            active_service_request.sr_state_id = active_service_state.sr_state_id
-
-            line = line + 1
-            print("    --> Line: " + str(line))
-            db.session.add(citizen)
-            line = line + 1
-            print("    --> Line: " + str(line))
-            db.session.commit()
-
-            line = line + 1
-            print("    --> Line: " + str(line))
-            socketio.emit('update_customer_list', {}, room=csr.office_id)
-            line = line + 1
-            print("    --> Line: " + str(line))
-            socketio.emit('citizen_invited', {}, room='sb-%s' % csr.office.office_number)
-            line = line + 1
-            print("    --> Line: " + str(line))
-            result = self.citizen_schema.dump(citizen)
-            line = line + 1
-            print("    --> Line: " + str(line))
-            socketio.emit('update_active_citizen', result.data, room=csr.office_id)
-
-        line = line + 1
-        print("    --> Line: " + str(line))
-        return {'citizen': result.data,
-                'errors': result.errors}, 200
+        return {'citizen': "None",
+                'errors': "None"}, 200
