5001fded22769ea31a5de5f6655ccb52f531d103
ozamani <ozamani9@gmail.com>
2020-04-17 14:47:36 -0700

    fix vulnerabilities - fix spinner in IE

--- frontend/src/add-citizen/form-components/tables.vue
+++ frontend/src/add-citizen/form-components/tables.vue
@@ -231,6 +231,14 @@
 </script>
 
 <style>
+   @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
   #navi {
     position: relative;
   }
--- frontend/src/appointments/checkin-modal.vue
+++ frontend/src/appointments/checkin-modal.vue
@@ -137,6 +137,14 @@
 </script>
 
 <style scoped>
+   @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
   #navi {
     position: relative;
   }
