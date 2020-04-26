0fe161c138574aeb2a21531a7a6e7fa471760879
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2020-04-25 23:32:13 -0700

    Changes to allow conflict for blackout events

--- api/app/schemas/theq/__init__.py
+++ api/app/schemas/theq/__init__.py
@@ -22,6 +22,7 @@ from .smartboard_schema import SmartBoardSchema
 from .role_schema import RoleSchema
 from .service_schema import ServiceSchema
 from .timezone_schema import TimezoneSchema
+from .timeslot_schema import TimeslotSchema
 from .office_schema import OfficeSchema
 from .csr_schema import CSRSchema
 from .metadata_schema import MetaDataSchema
