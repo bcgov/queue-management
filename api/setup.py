2ca3d5c7b9875c506a93c75e26263913067fe182
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2020-04-27 18:03:24 -0700

    Adding setup.py

--- api/setup.py
+++ api/setup.py
@@ -47,7 +47,7 @@ def read(filepath):
 REQUIREMENTS = read_requirements('requirements.txt')
 
 setup(
-    name="app",
+    name="theq_api",
     packages=find_packages('app'),
     package_dir={'': 'app'},
     py_modules=[splitext(basename(path))[0] for path in glob('app/*.py')],
