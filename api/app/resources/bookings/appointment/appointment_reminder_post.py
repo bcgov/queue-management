5221071bfba5285f84e34b62e6de5c27f265f81d
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2020-04-30 00:32:00 -0700

    Changes for email service and job

--- api/app/resources/bookings/appointment/appointment_reminder_post.py
+++ api/app/resources/bookings/appointment/appointment_reminder_post.py
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.'''
 
-from flask import abort
+from flask import abort, g
 from flask_restx import Resource
 
 from app.models.bookings import Appointment
