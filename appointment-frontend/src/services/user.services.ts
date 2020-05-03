499fdff33ddcf2d0665d5057bfbea102e9f727b1
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2020-05-02 17:26:31 -0700

    user get

--- appointment-frontend/src/services/user.services.ts
+++ appointment-frontend/src/services/user.services.ts
@@ -6,6 +6,10 @@ import { addAxiosInterceptors } from '@/utils/interceptors'
 const axios = addAxiosInterceptors(Axios.create())
 
 export default class UserService {
+  public static async getUser (): Promise<AxiosResponse<User[]>> {
+    return axios.get(`${ConfigHelper.getAppAPIUrl()}/users/me/`)
+  }
+
   public static async createUser (): Promise<AxiosResponse<User[]>> {
     return axios.post(`${ConfigHelper.getAppAPIUrl()}/users/`, {})
   }
