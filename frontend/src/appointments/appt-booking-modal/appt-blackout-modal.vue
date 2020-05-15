e57b1c3ba2715c83d1a983c5e9524c5dca404b98
ozamani9 <ozamani9@gmail.com>
2020-05-15 12:45:29 -0700

    Appt and blackout code (#398)
    * merge appt changes and commenting blackout yearly/monthly

--- frontend/src/appointments/appt-booking-modal/appt-blackout-modal.vue
+++ frontend/src/appointments/appt-booking-modal/appt-blackout-modal.vue
@@ -279,8 +279,8 @@
               <b-form-checkbox-group id="frequency-checkboxes"
                                      v-model="selected_frequency"
                                      @input="checkRecurringInput">
-                <b-form-checkbox :value="yearly">Yearly</b-form-checkbox>
-                <b-form-checkbox :value="monthly">Monthly</b-form-checkbox>
+<!--                <b-form-checkbox :value="yearly">Yearly</b-form-checkbox>-->
+<!--                <b-form-checkbox :value="monthly">Monthly</b-form-checkbox>-->
                 <b-form-checkbox :value="weekly">Weekly</b-form-checkbox>
                 <b-form-checkbox :value="daily">Daily</b-form-checkbox>
               </b-form-checkbox-group>
