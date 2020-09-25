ef4479632be142bf67204b0dd50fc91969cfe393
Adam Coard <arcoard+github@gmail.com>
2020-09-25 11:23:05 -0700

    Fix lint issue causing compile error

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,6 +1,7 @@
+/*eslint-disable */
 const config = {
     APP_API_URL: process.env.VUE_APP_API_URL || '/api/v1/',
-    SOCKET_URL: process.env.VUE_APP_SOCKET_URL  || '',
+    SOCKET_URL: process.env.VUE_APP_SOCKET_URL  || ''
 }
 
 export default { ...config }
