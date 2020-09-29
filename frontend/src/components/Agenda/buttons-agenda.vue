8bbfc8f3a548e54ea1f27c776674e6b0eb2eb9fe
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2020-09-29 14:22:57 -0700

    Agenda/Agenda.vue | Agenda/buttons-agenda.vue

--- frontend/src/components/Agenda/buttons-agenda.vue
+++ frontend/src/components/Agenda/buttons-agenda.vue
@@ -2,33 +2,33 @@
   <div>
     <form inline>
       <b-button class="btn-primary" @click="prev">
-        <font-awesome-icon icon="angle-left"
-                           class="m-0 p-0"
-                           style="font-size: 1rem;"/>
+        <font-awesome-icon icon="angle-left" class="m-0 p-0" style="font-size: 1rem;" />
       </b-button>
-      <b-button class="btn-primary" @click="next">
-        <font-awesome-icon icon="angle-right"
-                           class="m-0 p-0"
-                           style="font-size: 1rem;"/>
+      <b-button class="btn-primary ml-1" @click="next">
+        <font-awesome-icon icon="angle-right" class="m-0 p-0" style="font-size: 1rem;" />
       </b-button>
       <b-button class="btn-primary mx-2" @click="today">This Week</b-button>
     </form>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'ButtonsAgenda',
-  methods: {
-    next () {
-      this.$root.$emit('agenda-next')
-    },
-    prev () {
-      this.$root.$emit('agenda-prev')
-    },
-    today () {
-      this.$root.$emit('agenda-today')
-    }
+<script lang="ts">
+// /* eslint-disable */
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class ButtonsAgenda extends Vue {
+  next () {
+    this.$root.$emit('agenda-next')
+  }
+
+  prev () {
+    this.$root.$emit('agenda-prev')
+  }
+
+  today () {
+    this.$root.$emit('agenda-today')
   }
 }
+
 </script>
