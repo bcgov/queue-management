d2b9d59a5ed1a74a509921efdb5f545ffb6db0dc
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2020-10-02 11:01:23 -0700

    axios changes

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -19,44 +19,6 @@ export function Axios (context) {
         config.headers.common.Authorization = `Bearer ${bearer}`
         return config
     })
-
-    // return (
-    //     axios.create({
-    //         baseURL: URLConfig.APP_API_URL,
-    //         withCredentials: true,
-    //         headers: {
-    //             Accept: 'application/json',
-    //             Authorization: `Bearer ${bearer}`
-    //         }
-    //     })
-    // )
 }
-
-// instance.interceptors.request.use((config) => {
-//     config.headers.get['Content-Type'] = 'application/json'
-//     config.headers.post['Content-Type'] = 'application/json'
-//     config.headers.put['Content-Type'] = 'application/json'
-//     config.headers.patch['Content-Type'] = 'application/json'
-//     config.headers.delete['Content-Type'] = 'application/json'
-//     config.headers.common.Authorization = `Bearer ${sessionStorage.getItem(
-//         'keycloak_token'
-//     )}`
-//     return config
-// })
-
-// Add a response interceptor
-// instance.interceptors.response.use(
-//     (response) => {
-//         return response
-//     },
-//     async (error) => {
-//         // redirect unathorized request to unathorized page
-//         if (error.response.status === 401) {
-//             store.dispatch('ProjectInfoModule/resetprogress')
-//             await router.push('/unauthorized')
-//         }
-//         return Promise.reject(error)
-//     }
-// )
 
 export default instance
