13ce976fda216e7c79b9d27c302fa0cacd513c4c
Adam Coard <arcoard+github@gmail.com>
2020-12-08 09:53:05 -0800

    Add automatic versioning script

--- frontend/src/components/Layout/footer.vue
+++ frontend/src/components/Layout/footer.vue
@@ -48,7 +48,8 @@
           class="footer-anchor-item-last"
           style="display: inline-block; color: white; margin-right: 15px"
         >
-          v2.0.1
+          <!-- v2.0.1 ({{ version }}) -->
+          {{ version.projectVersion }} ({{ version.gitCommit }})
         </div>
       </div>
     </div>
@@ -58,6 +59,8 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import SchedulingIndicator from '../Booking/scheduling-indicator.vue'
+import * as version from '../../version.GENERATED';
+
 
 @Component({
   components: {
@@ -69,6 +72,14 @@ export default class Footer extends Vue {
   keycloakLogin () {
     this.$keycloak.login()
   }
+
+  mounted() {
+    version.success
+            ? console.log('%c' + version.message, 'font-size: 20px;')
+            : console.error(version.message);
+  }
+
+  version = version;
 }
 </script>
 
