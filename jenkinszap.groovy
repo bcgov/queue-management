3f7fdd1c693dde5eb4a6a95171a930291cb51b68
josekudiyirippil <jose.kudiyirippil@bc.gov.ca>
2021-01-18 11:34:19 -0800

    zap update

--- jenkinszap.groovy
+++ jenkinszap.groovy
@@ -28,8 +28,6 @@ def API_IMAGE_HASH = ""
 def FRONTEND_IMAGE_HASH = ""
 def APPOINTMENT_IMAGE_HASH = ""
 def REMINDER_IMAGE_HASH = ""
-def owaspPodLabel = "jenkins-agent-zap"
-
 
 String getNameSpace() {
     def NAMESPACE = sh (
@@ -279,48 +277,6 @@ podTemplate(
         }
     }
 }
-podTemplate(
-    label: owaspPodLabel, 
-    name: owaspPodLabel, 
-    serviceAccount: 'jenkins', 
-    cloud: 'openshift', 
-    containers: [ containerTemplate(
-        name: 'jenkins-agent-zap',
-        image: 'image-registry.openshift-image-registry.svc:5000/5c0dde-tools/jenkins-agent-zap:latest',
-        resourceRequestCpu: '500m',
-        resourceLimitCpu: '1000m',
-        resourceRequestMemory: '3Gi',
-        resourceLimitMemory: '4Gi',
-        workingDir: '/home/jenkins',
-        command: '',
-        args: '${computer.jnlpmac} ${computer.name}'
-    )]
-) {
-    node(owaspPodLabel) {
-        stage('ZAP Security Scan') {          
-            def retVal = sh (
-                returnStatus: true, 
-                script: "/zap/zap-baseline.py -r index.html -t https://dev-qms.apps.silver.devops.gov.bc.ca"
-            )
-            publishHTML([
-                allowMissing: false, 
-                alwaysLinkToLastBuild: false, 
-                keepAll: true, 
-                reportDir: '/zap/wrk', 
-                reportFiles: 'index.html', 
-                reportName: 'OWASPReport', 
-            ])
-            echo "Return value is: ${retVal}"
-
-            script {
-                if (retVal != 0) {
-                    echo "MARKING BUILD AS UNSTABLE"
-                    currentBuild.result = 'UNSTABLE'
-                }
-            }
-        }
-    }
-  }
 node {
     stage("Deploy to test") {
         input "Deploy to test?"
