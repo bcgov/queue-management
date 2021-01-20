c48a719b0d246fd9e44620e5e888f0e8e590d428
josekudiyirippil <jose.kudiyirippil@bc.gov.ca>
2021-01-20 14:00:52 -0800

    zap update

--- zaptest.groovy
+++ zaptest.groovy
@@ -34,6 +34,24 @@ podTemplate(
         }
        }
     }}
+    def owaspPodLabel = "jenkins-agent-zap"
+podTemplate(
+    label: owaspPodLabel, 
+    name: owaspPodLabel, 
+    serviceAccount: 'jenkins', 
+    cloud: 'openshift', 
+    containers: [ containerTemplate(
+        name: 'jenkins-agent-zap',
+        image: 'image-registry.openshift-image-registry.svc:5000/5c0dde-tools/jenkins-agent-zap:latest',
+        resourceRequestCpu: '500m',
+        resourceLimitCpu: '1000m',
+        resourceRequestMemory: '3Gi',
+        resourceLimitMemory: '4Gi',
+        workingDir: '/home/jenkins',
+        command: '',
+        args: '${computer.jnlpmac} ${computer.name}'
+    )]
+)
     node(owaspPodLabel) {
         zap_scan_appointment:{
             stage('ZAP Security Scan') {          
