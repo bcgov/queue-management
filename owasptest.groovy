62decb8fa6b6f2dd7bd8e787b0a2613caca7ab3c
josekudiyirippil <jose.kudiyirippil@bc.gov.ca>
2021-01-21 10:27:12 -0800

    zap update

--- owasptest.groovy
+++ owasptest.groovy
@@ -22,28 +22,20 @@ podTemplate(
                 returnStatus: true, 
                 script: "/zap/zap-baseline.py -r index1.html -t https://dev-qms.apps.silver.devops.gov.bc.ca/"
             )
-            publishHTML([
-                allowMissing: false, 
-                alwaysLinkToLastBuild: false, 
-                keepAll: true, 
-                reportDir: '/zap/wrk', 
-                reportFiles: 'index1.html', 
-                reportName: 'OWASPReport', 
-            ])
         }
         stage('ZAP Security Scan') {          
                 def retVal = sh (
                     returnStatus: true, 
                     script: "/zap/zap-baseline.py -r index2.html -t https://dev-qmsappointments.apps.silver.devops.gov.bc.ca/appointment/",
                 )
-                sh 'echo "<html><head></head><body><a href=index1.html>Staff Front Report</a><br><a href=index2.html>Appoint Front End Report</a></body></html>" > /zap/wrk/index.html'
+                sh 'echo "<html><head></head><body><a href=index1.html>Staff Front Report</a><br><a href=index2.html>Appointment Front End Report</a></body></html>" > /zap/wrk/index.html'
                 publishHTML([
                     allowMissing: false, 
                     alwaysLinkToLastBuild: true, 
                     keepAll: true, 
                     reportDir: '/zap/wrk', 
                     reportFiles: 'index.html', 
-                    reportName: 'OWASPReportappointment', 
+                    reportName: 'OWASPReport', 
                 ])
                 echo "Return value is: ${retVal}"
 
