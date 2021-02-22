789c050b9f49d892f58d7f3b97b80f58f54fa14f
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2021-02-22 13:39:24 -0800

    Feedback API Development
    Feedback API Development

---
+++ feedback-api/setup.py
@@ -0,0 +1,59 @@
+# Copyright © 2019 Province of British Columbia.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Installer and setup for this module
+"""
+from glob import glob
+from os.path import basename, splitext
+
+from setuptools import find_packages, setup
+
+
+def read_requirements(filename):
+    """
+    Get application requirements from
+    the requirements.txt file.
+    :return: Python requirements
+    :rtype: list
+    """
+    with open(filename, 'r') as req:
+        requirements = req.readlines()
+    install_requires = [r.strip() for r in requirements if r.find('git+') != 0]
+    return install_requires
+
+
+def read(filepath):
+    """
+    Read the contents from a file.
+    :param str filepath: path to the file to be read
+    :return: file contents
+    :rtype: str
+    """
+    with open(filepath, 'r') as file_handle:
+        content = file_handle.read()
+    return content
+
+
+REQUIREMENTS = read_requirements('requirements.txt')
+
+setup(
+    name="api",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
