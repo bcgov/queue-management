2705ca45669b2be1317b4405f0f70b2c85171742
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2021-03-12 08:40:08 -0800

    Feedback API Code changes

--- feedback-api/src/api/services/feedback/feedback_camunda_service.py
+++ feedback-api/src/api/services/feedback/feedback_camunda_service.py
@@ -34,12 +34,35 @@ class FeedbackCamundaService(FeedbackBaseService):
         auth_payload = {"grant_type":"client_credentials",
                 "client_id":keycloak_client_id,
                 "client_secret":keycloak_client_secret}
-        auth_response = requests.post(keycloak_endpoint,data=auth_payload)
-        access_token = auth_response.json()['access_token']
-        headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {access_token}'}
-        feedback_response = requests.post(camunda_service_endpoint,
-                            headers=headers,
-                            data=json.dumps(payload))
-        print(feedback_response.json())
-        return feedback_response.status_code
+        try:
+            auth_response = requests.post(keycloak_endpoint,data=auth_payload)
+            access_token = auth_response.json()['access_token']
+            headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {access_token}'}
+            feedback_response = requests.post(camunda_service_endpoint,
+                                headers=headers,
+                                data=json.dumps(payload), timeout=5.0)
+            return feedback_response.status_code
+        except Exception as e:
+            application_auth_url = os.getenv('APP_AUTH_URL')
+            application_client_id = os.getenv('APP_AUTH_CLIENT_ID')
+            application_client_secret = os.getenv('APP_AUTH_CLIENT_SECRET')
+            notification_email_url = os.getenv('NOTIFICATION_EMAIL_URL')
+            email_to = (os.getenv('NOTIFICATION_EMAIL_TO')).split(",")
+            app_auth_payload = {"grant_type":"client_credentials",
+                "client_id":application_client_id,
+                "client_secret":application_client_secret}
+            email_payload = {
+                'bodyType': 'text',
+                'body': json.dumps(payload),
+                'subject': 'Citizen Feedback - Camunda API failure',
+                'to': email_to
+            }
+            app_auth_response = requests.post(application_auth_url,data=app_auth_payload)
+            app_access_token = app_auth_response.json()['access_token']
+            email_headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {app_access_token}'}
+            email_response = requests.post(notification_email_url,
+                                headers=email_headers,
+                                data=json.dumps(email_payload))
+            print(email_response)
+            print(e)  # log and continue
         
