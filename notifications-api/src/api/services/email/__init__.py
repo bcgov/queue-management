e0695357470d00820e490a0a0ea6fdbe08df6774
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2021-03-16 08:59:43 -0700

    Email flag updated

--- notifications-api/src/api/services/email/__init__.py
+++ notifications-api/src/api/services/email/__init__.py
@@ -21,8 +21,10 @@ def get_email_service():
     from .email_ches_notify import EmailChesNotify
 
     _instance: EmailBaseService = None
-    if os.getenv('EMAIL_USE_GC_NOTIFY', 'false').lower() == 'true':
+    if os.getenv('EMAIL_PROVIDER') == 'GC_NOTIFY':
         _instance = EmailGCNotify()
-    else:
+    elif os.getenv('EMAIL_PROVIDER') == 'CHES':
         _instance = EmailChesNotify()
+    else:
+        _instance = EmailGCNotify()
     return _instance
