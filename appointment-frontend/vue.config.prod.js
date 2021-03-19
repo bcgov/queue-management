b9c22d1f8813a8764e84c592376c14374fadc826
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2021-03-19 12:23:22 -0700

    Added prod config

---
+++ appointment-frontend/vue.config.prod.js
@@ -0,0 +1,30 @@
+var path = require('path')
+module.exports = {
+  configureWebpack: {
+    devtool: false,
+    resolve: {
+      alias: {
+        'vue': path.resolve('./node_modules/vue'),
+        '$assets': path.resolve('./src/assets/')
+      }
+    }
+  },
+  // publicPath: process.env.VUE_APP_PATH,
+  transpileDependencies: ['vuetify', 'vuex-persist'],
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    // Configure local API calls to hit OpenShift dev
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug'
+      }
+    },
+    // Allow using the local proxy across other devices on LAN
+    headers: { 'Access-Control-Allow-Origin': '*' }
+  }
+}
