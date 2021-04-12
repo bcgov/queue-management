3298394cc62b1888349cf73de6a516bc844be791
josekudiyirippil <jose.kudiyirippil@bc.gov.ca>
2021-04-12 16:39:26 -0700

    zap test

--- Jenkinswithzap.groovy
+++ Jenkinswithzap.groovy
@@ -31,6 +31,8 @@ def REMINDER_IMAGE_HASH = ""
 def NOTIFICATION_IMAGE_HASH = ""
 def FEEDBACK_IMAGE_HASH = ""
 def owaspPodLabel = "jenkins-agent-zap"
+def STAFFURL = ""
+def APPTMNTURL = ""
 
 String getNameSpace() {
     def NAMESPACE = sh (
@@ -389,6 +391,19 @@ podTemplate(
         }
     }
 }
+node() {
+
+         stage('get url') {
+		        STAFFURL = sh (
+                    script: 'oc describe configmap jenkin-config | awk  -F  "=" \'/^zap_url_staff/{print $2}\'',
+                    returnStdout: true
+                ).trim()
+		        APPTMNTURL = sh (
+                    script: 'oc describe configmap jenkin-config | awk  -F  "=" \'/^zap_url_appntmnt/{print $2}\'',
+                    returnStdout: true
+                ).trim()				
+        }
+	}
 podTemplate(
     label: owaspPodLabel, 
     name: owaspPodLabel, 
@@ -410,13 +425,13 @@ podTemplate(
         stage('ZAP Security Scan') {          
             def retVal = sh (
                 returnStatus: true, 
-                script: "/zap/zap-baseline.py -r index1.html -t $q_url",
+                script: "/zap/zap-baseline.py -r index1.html -t ${STAFFURL}",
             )
         }
         stage('ZAP Security Scan') {          
                 def retVal = sh (
                     returnStatus: true, 
-                    script: "/zap/zap-baseline.py -r index2.html -t $appmnt_url",
+                    script: "/zap/zap-baseline.py -r index2.html -t ${APPTMNTURL}",
                 )
                 sh 'echo "<html><head></head><body><a href=index1.html>Staff Front Report</a><br><a href=index2.html>Appointment Front End Report</a></body></html>" > /zap/wrk/index.html'
                 publishHTML([
