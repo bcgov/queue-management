3a25f8c865d1f6edd7256b0b1ced98a1775423b8
josekudiyirippil <jose.kudiyirippil@bc.gov.ca>
2021-04-12 10:32:49 -0700

    zap update

---
+++ zaptest.groovy
@@ -0,0 +1,68 @@
+def owaspPodLabel = "jenkins-agent-zap"
+String getNameSpace() {
+    def NAMESPACE = sh (
+        script: 'oc describe configmap jenkin-config | awk  -F  "=" \'/^namespace/{print $2}\'',
+        returnStdout: true
+    ).trim()
+    return NAMESPACE
+}
+
+// Get an image's hash tag
+String getImageTagHash(String imageName, String tag = "") {
+
+  if(!tag?.trim()) {
+    tag = "latest"
+  }
+
+  def istag = openshift.raw("get istag ${imageName}:${tag} -o template --template='{{.image.dockerImageReference}}'")
+  return istag.out.tokenize('@')[1].trim()
+}
+podTemplate(
+    label: owaspPodLabel, 
+    name: owaspPodLabel, 
+    serviceAccount: 'jenkins', 
+    cloud: 'openshift', 
+    containers: [ containerTemplate(
+        name: 'jenkins-agent-zap',
+        image: 'image-registry.openshift-image-registry.svc:5000/5c0dde-tools/jenkins-agent-zap:latest',
+        resourceRequestCpu: '500m',
+        resourceLimitCpu: '1000m',
+        resourceRequestMemory: '3Gi',
+        resourceLimitMemory: '4Gi',
+        workingDir: '/home/jenkins',
+        command: '',
+        args: '${computer.jnlpmac} ${computer.name}'
+    )]
+) {
+    node(owaspPodLabel) {
+        stage('ZAP Security Scan') {          
+            def retVal = sh (
+                returnStatus: true, 
+                script: "/zap/zap-baseline.py -r index1.html -t $q_url"
+            )
+        }
+        stage('ZAP Security Scan') {          
+                def retVal = sh (
+                    returnStatus: true, 
+                    script: "/zap/zap-baseline.py -r index2.html -t $appmnt_url",
+                )
+                sh 'echo "<html><head></head><body><a href=index1.html>Staff Front Report</a><br><a href=index2.html>Appointment Front End Report</a></body></html>" > /zap/wrk/index.html'
+                publishHTML([
+                    allowMissing: false, 
+                    alwaysLinkToLastBuild: true, 
+                    keepAll: true, 
+                    reportDir: '/zap/wrk', 
+                    reportFiles: 'index.html', 
+                    reportName: 'OWASPReport', 
+                ])
+                echo "Return value is: ${retVal}"
+
+                script {
+                    if (retVal != 0) {
+                        echo "MARKING BUILD AS UNSTABLE"
+                        currentBuild.result = 'UNSTABLE'
+                    }
+                }
+        }
+    }
+  }
