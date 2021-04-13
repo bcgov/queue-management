ae2d6177fa2dc6b990d1d865d44e05257ee0a4c1
josekudiyirippil <jose.kudiyirippil@bc.gov.ca>
2021-04-13 09:26:26 -0700

    zap test

--- Jenkinswithzap.groovy
+++ Jenkinswithzap.groovy
@@ -91,7 +91,7 @@ podTemplate(
                     script: "pwd",
                     returnStdout: true
             ).trim()
-            SONAR_SOURCES = 'api,frontend,appointment-frontend,jobs'
+            SONAR_SOURCES = 'api,frontend,appointment-frontend,jobs,feedback-api,notifications-api'
             SONARQUBE_PWD = sh (
                 script: 'oc describe configmap jenkin-config | awk  -F  "=" \'/^sonarqube_key/{print $2}\'',
                 returnStdout: true
