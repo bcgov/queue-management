c8d3821e4e10a457866ab963d9361baf4957d5d6
Ann <ann.jacob@aot-technologies.com>
2021-04-14 20:12:11 -0400

    D

--- frontend/src/views/FormView.vue
+++ frontend/src/views/FormView.vue
@@ -1,17 +1,16 @@
 <template>
-    <div>
-        <FormViewSubmission
-          v-if="form_id && submission_id"
-          :formid="form_id"
-          :submissionid="submission_id"
-        />
-    </div>
+  <div>
+    <FormViewSubmission
+      v-if="form_id && submission_id"
+      :formid="form_id"
+      :submissionid="submission_id"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { Component,  Vue } from 'vue-property-decorator'
-import FormViewSubmission from 'camunda-formio-tasklist-vue/src/components/FormViewSubmission.vue'
-import configMap from '../utils/config-helper'
+import { Component, Prop, Vue } from "vue-property-decorator";
+import FormViewSubmission from "camunda-formio-tasklist-vue/src/components/FormViewSubmission.vue";
 
 @Component({
   components: {
@@ -19,14 +18,12 @@ import configMap from '../utils/config-helper'
   }
 })
 export default class FormView extends Vue {
-  private form_id: string = ''
-  private submission_id: string = ''
-  public isServiceFLowEnabled = configMap.isServiceFLowEnabled()
+  private form_id: string = "";
+  private submission_id: string = "";
 
-  mounted () {
-    this.form_id = this.$route.params.form_id
+  mounted() {
+    this.form_id = this.$route.params.form_id;
     this.submission_id = this.$route.params.submission_id;
-    this.isServiceFLowEnabled = configMap.isServiceFLowEnabled()
   }
 }
 </script>
