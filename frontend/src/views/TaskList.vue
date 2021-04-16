cf8ac101b3c2516ec21e909a1ac8c244df1fd253
Ann <ann.jacob@aot-technologies.com>
2021-04-15 21:21:33 -0400

    tasklist.vue renamed

--- frontend/src/views/TaskList.vue
+++ frontend/src/views/TaskList.vue
@@ -19,7 +19,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator'
+import { Component, Vue ,Watch } from 'vue-property-decorator'
 import CamundaTasklist from 'camunda-formio-tasklist-vue/src/components/TaskList.vue'
 import { State } from 'vuex-class'
 import configMap from '../utils/config-helper'
