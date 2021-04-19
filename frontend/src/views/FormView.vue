b54ec5ef098b3cacadaa677bcf498725e522884b
ann-aot <77353155+ann-aot@users.noreply.github.com>
2021-04-19 13:48:08 -0400

    Update FormView.vue

--- frontend/src/views/FormView.vue
+++ frontend/src/views/FormView.vue
@@ -2,8 +2,8 @@
     <div>
         <FormViewSubmission
           v-if="form_id && submission_id"
-          :formid = form_id
-          :submissionid = submission_id
+          :formid=form_id
+          :submissionid=submission_id
         />
     </div>
 </template>
