777a0528ccbef4cf929c31c69478b943be84203b
Ann <ann.jacob@aot-technologies.com>
2021-05-06 20:53:19 -0400

    websocket encrypt key added

--- frontend/src/views/TaskList.vue
+++ frontend/src/views/TaskList.vue
@@ -12,7 +12,6 @@
       :formsflowaiUrl="configs.FORM_FLOW_URL"
       :formsflowaiApiUrl="configs.FORM_FLOW_API_URL"
       :webSocketEncryptkey="configs.WEBSOCKET_ENCRYPT_KEY"
-      :getTaskId="getTaskId"
     />
     <div class="no-content" v-else>You shouldnot be here !!!</div>
   </div>
