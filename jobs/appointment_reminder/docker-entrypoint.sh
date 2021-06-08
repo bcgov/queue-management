847f9682b7fac80bea47685ce541a1c4f51221b0
WalterMoar <Walter.Moar@gov.bc.ca>
2021-06-08 15:53:36 -0700

    Reverted crond verbose flag in case it's blocking logging

--- jobs/appointment_reminder/docker-entrypoint.sh
+++ jobs/appointment_reminder/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 function start_cron_jobs() {
   echo "Starting go-crond as a background task ..."
-  CRON_CMD="go-crond --allow-unprivileged --include=cron/"
+  CRON_CMD="go-crond -v --allow-unprivileged --include=cron/"
   exec ${CRON_CMD}
 }
 
