66b36456f641e555786222dc7e80542d17cd7a0a
Thant Zin Oo <thantzin.oo@avocette.com>
2021-07-14 16:15:04 -0700

    refactor: consolidate toggling codes, add comments

--- api/static/js/office.js
+++ api/static/js/office.js
@@ -1,150 +1,131 @@
-console.log('office.js loaded from api/static/js');
-
 $(document).ready(function() {
     // Smartboard
     var sb = $("#sb"); 
-    if (sb.val() === "3") { // if nocallonsmartboard
+    if (sb.val() === "__None" || sb.val() === "3") { // if not selected or nocallonsmartboard
         toggleSmartboardFields(false);
     }
-    sb.on('change', function (e) {        
-        if (e.val === "3") { // if nocallonsmartboard
-            console.log('smartboard value is nocallonsmartboard');
+    sb.on('change', (e) => {        
+        if (e.val === "__None" || e.val === "3") { // if not selected or nocallonsmartboard
             toggleSmartboardFields(false);     
-        } else {            
-            console.log('smartboard value is NOT nocallonsmartboard');
+        } else { // if selected and NOT nocallonsmartboard            
             toggleSmartboardFields(true);
         }
     })
 
     // Digital Signage
     var digital_signage_message = $("#digital_signage_message");
-    if (digital_signage_message.val() === "__None" || digital_signage_message.val() === "0") {
+    if (digital_signage_message.val() === "__None" || digital_signage_message.val() === "0") { // if not selected or disabled
         toggleDigitalSignageFields(false);
     }
-    digital_signage_message.on('change', function(e) {
-        if (e.val === "0") { // if disabled
+    digital_signage_message.on('change', (e) => {
+        if (e.val  === "__None" || e.val === "0") { // if not selected or disabled
             toggleDigitalSignageFields(false);
-        } else {
+        } else { // if enabled
             toggleDigitalSignageFields(true);
         }
     });
 
     // Check-In Notifications
     var check_in_notification = $("#check_in_notification");
-    if (check_in_notification.val() === "__None" || check_in_notification.val() === "0") {
+    if (check_in_notification.val() === "__None" || check_in_notification.val() === "0") { // if not selected or disabled
         toggleCheckInNotificationFields(false);
     }
-    check_in_notification.on('change', function(e) {
-        if (e.val === "0") { //if disabled
+    check_in_notification.on('change', (e) => { 
+        if (e.val === "__None" || e.val === "0") { // if not selected or disabled
             toggleCheckInNotificationFields(false);
-        } else {
+        } else { // if enabled
             toggleCheckInNotificationFields(true);
         }
     });
     
     // Appointments Enabled
     var appointments_enabled_ind = $("#appointments_enabled_ind");
-    if (appointments_enabled_ind.val() === "0") {
+    if (appointments_enabled_ind.val() === "0") { // if disabled
         toggleAppointmentFields(false);
         toggleOnlineStatusFields(false);
     }        
-    appointments_enabled_ind.on('change', function (e) {        
-        if (e.val === "0") { // if disabled
-            console.log('appointments disabled')
+    appointments_enabled_ind.on('change', (e) => {        
+        if (e.val === "0") { // if disabled            
             toggleAppointmentFields(false);
             toggleOnlineStatusFields(false);
-        } else { // if enabled
-            console.log('appointments enabled')
+        } else { // if enabled            
             toggleAppointmentFields(true);
         }
     });
     
     // Online Status
     var online_status = $("#online_status");
-    if (online_status.val() === "__None" || online_status.val() === "HIDE") {
+    if (online_status.val() === "__None" || online_status.val() === "HIDE") { // if not selected or HIDE
         toggleOnlineStatusFields(false);
     }        
-    online_status.on('change', function (e) {        
-        if (e.val === "HIDE") { // if Hidden
+    online_status.on('change', (e) => {        
+        if (e.val === "__None" || e.val === "HIDE") { // if not selected or HIDE
             toggleOnlineStatusFields(false);
-        } else { // if NOT Hidden
-            if (appointments_enabled_ind.val() === "0") {
+        } else { // if SHOW
+            if (appointments_enabled_ind.val() === "0") { // if appointment is disabled, hide Online Status fields still
                 toggleOnlineStatusFields(false);
-            } else {
+            } else { // if appointment is enabled, show Online Status fields
                 toggleOnlineStatusFields(true);
             }            
         }
     });
 });
 
+function toggleParentContainer(el, flag) {
+    if (flag)
+        el.parent().show();
+    else
+        el.parent().hide();
+}
+
+function toggleParentContainers(arr, flag) {
+    arr.forEach((el, index) => {  
+        toggleParentContainer(el, flag);
+    });
+}
+
 function toggleSmartboardFields(flag) {
-    if (flag) {
-        $("label[for='currently_waiting']").parent().show();
-        $("label[for='show_currently_waiting_bottom']").parent().show();
-    } else {
-        $("label[for='currently_waiting']").parent().hide();
-        $("label[for='show_currently_waiting_bottom']").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='currently_waiting']"));
+    arr.push($("label[for='show_currently_waiting_bottom']"));
+    toggleParentContainers(arr, flag);
 }
 
 function toggleDigitalSignageFields(flag) {
-    if (flag) {
-        $("label[for='digital_signage_message_1']").parent().show();
-        $("label[for='digital_signage_message_2']").parent().show();
-        $("label[for='digital_signage_message_3']").parent().show();
-    } else {
-        $("label[for='digital_signage_message_1']").parent().hide();
-        $("label[for='digital_signage_message_2']").parent().hide();
-        $("label[for='digital_signage_message_3']").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='digital_signage_message_1']"));
+    arr.push($("label[for='digital_signage_message_2']"));
+    arr.push($("label[for='digital_signage_message_3']"));
+    toggleParentContainers(arr, flag);
 }
 
 function toggleCheckInNotificationFields(flag) {
-    if (flag) {
-        $("label[for='check_in_reminder_msg']").parent().show();
-        $("label[for='automatic_reminder_at']").parent().show();
-    } else {
-        $("label[for='check_in_reminder_msg']").parent().hide();
-        $("label[for='automatic_reminder_at']").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='check_in_reminder_msg']"));
+    arr.push($("label[for='automatic_reminder_at']"));
+    toggleParentContainers(arr, flag);
 }
 
 function toggleAppointmentFields(flag) {
-    if (flag) {
-        $("label[for='appointment_duration']").parent().show();
-        $("label[for='office_email_paragraph'").parent().show();
-        $("label[for='online_status'").parent().show();
-    } else {
-        $("label[for='appointment_duration']").parent().hide();
-        $("label[for='office_email_paragraph'").parent().hide();
-        $("label[for='online_status'").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='appointment_duration']"));
+    arr.push($("label[for='office_email_paragraph']"));
+    arr.push($("label[for='online_status']"));
+    toggleParentContainers(arr, flag);
 }
 
 function toggleOnlineStatusFields(flag) {
-    if (flag) {
-        $("label[for='office_appointment_message']").parent().show();
-        $("label[for='latitude']").parent().show();
-        $("label[for='longitude']").parent().show();
-        $("label[for='civic_address']").parent().show();
-        $("label[for='telephone']").parent().show();
-        $("label[for='external_map_link']").parent().show();
-        $("label[for='appointments_days_limit']").parent().show();
-        $("label[for='soonest_appointment']").parent().show();
-        $("label[for='max_person_appointment_per_day']").parent().show();
-        $("label[for='number_of_dlkt']").parent().show();
-        $("label[for='timeslots']").parent().show();
-    } else {
-        $("label[for='office_appointment_message']").parent().hide();
-        $("label[for='latitude']").parent().hide();
-        $("label[for='longitude']").parent().hide();
-        $("label[for='civic_address']").parent().hide();
-        $("label[for='telephone']").parent().hide();
-        $("label[for='external_map_link']").parent().hide();
-        $("label[for='appointments_days_limit']").parent().hide();
-        $("label[for='soonest_appointment']").parent().hide();
-        $("label[for='max_person_appointment_per_day']").parent().hide();
-        $("label[for='number_of_dlkt']").parent().hide();
-        $("label[for='timeslots']").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='office_appointment_message']"));
+    arr.push($("label[for='latitude']"));
+    arr.push($("label[for='longitude']"));
+    arr.push($("label[for='civic_address']"));
+    arr.push($("label[for='telephone']"));
+    arr.push($("label[for='external_map_link']"));
+    arr.push($("label[for='appointments_days_limit']"));
+    arr.push($("label[for='soonest_appointment']"));
+    arr.push($("label[for='max_person_appointment_per_day']"));
+    arr.push($("label[for='number_of_dlkt']"));
+    arr.push($("label[for='timeslots']"));
+    toggleParentContainers(arr, flag);
 }
--- api/static/js/officega.js
+++ api/static/js/officega.js
@@ -1,38 +1,35 @@
-console.log('officega.js loaded from api/static/js');
-
 $(document).ready(function() {
     // Smartboard
     var sb = $("#sb"); 
-    if (sb.val() === "3") { // if nocallonsmartboard
+    if (sb.val() === "__None" || sb.val() === "3") { // if not selected or nocallonsmartboard
         toggleSmartboardFields(false);
-    } else {
+    } else { // if selected and NOT nocallonsmartboard   
         toggleSmartboardFields(true);
     }
     $("select[id='sb'].form-control").prop("disabled", true);
-
     
     // Digital Signage
     var digital_signage_message = $("#digital_signage_message");
-    if (digital_signage_message.val() === "__None" || digital_signage_message.val() === "0") {
+    if (digital_signage_message.val() === "__None" || digital_signage_message.val() === "0") { // if not selected or disabled
         toggleDigitalSignageFields(false);
     }
     digital_signage_message.on('change', function(e) {
-        if (e.val === "0") { // if disabled
+        if (e.val  === "__None" || e.val === "0") { // if not selected or disabled
             toggleDigitalSignageFields(false);
-        } else {
+        } else { // if enabled
             toggleDigitalSignageFields(true);
         }
     });
     
     // Check-In Notifications
     var check_in_notification = $("#check_in_notification");
-    if (check_in_notification.val() === "__None" || check_in_notification.val() === "0") {
+    if (check_in_notification.val() === "__None" || check_in_notification.val() === "0") { // if not selected or disabled
         toggleCheckInNotificationFields(false);
     }
     check_in_notification.on('change', function(e) {
-        if (e.val === "0") { //if disabled
+        if (e.val === "__None" || e.val === "0") { // if not selected or disabled
             toggleCheckInNotificationFields(false);
-        } else {
+        } else { // if enabled
             toggleCheckInNotificationFields(true);
         }
     });
@@ -41,7 +38,7 @@ $(document).ready(function() {
     var appointments_enabled_ind = $("#appointments_enabled_ind");
     var online_status = $("#online_status"); 
 
-    if (appointments_enabled_ind.val() === "0") {
+    if (appointments_enabled_ind.val() === "0") { // if disabled
         toggleAppointmentFields(false);
         toggleOnlineStatusFields(false);
     }        
@@ -70,68 +67,58 @@ $(document).ready(function() {
     
 });
 
+function toggleParentContainer(el, flag) {
+    if (flag)
+        el.parent().show();
+    else
+        el.parent().hide();
+}
+
+function toggleParentContainers(arr, flag) {
+    arr.forEach((el, index) => {  
+        toggleParentContainer(el, flag);
+    });
+}
+
 function toggleSmartboardFields(flag) {
-    if (flag) {
-        $("label[for='currently_waiting']").parent().show();
-        $("label[for='show_currently_waiting_bottom']").parent().show();
-    } else {
-        $("label[for='currently_waiting']").parent().hide();
-        $("label[for='show_currently_waiting_bottom']").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='currently_waiting']"));
+    arr.push($("label[for='show_currently_waiting_bottom']"));
+    toggleParentContainers(arr, flag);
 }
 
 function toggleDigitalSignageFields(flag) {
-    if (flag) {
-        $("label[for='digital_signage_message_1']").parent().show();
-        $("label[for='digital_signage_message_2']").parent().show();
-        $("label[for='digital_signage_message_3']").parent().show();
-    } else {
-        $("label[for='digital_signage_message_1']").parent().hide();
-        $("label[for='digital_signage_message_2']").parent().hide();
-        $("label[for='digital_signage_message_3']").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='digital_signage_message_1']"));
+    arr.push($("label[for='digital_signage_message_2']"));
+    arr.push($("label[for='digital_signage_message_3']"));
+    toggleParentContainers(arr, flag);
 }
 
 function toggleCheckInNotificationFields(flag) {
-    if (flag) {
-        $("label[for='check_in_reminder_msg']").parent().show();
-        $("label[for='automatic_reminder_at']").parent().show();
-    } else {
-        $("label[for='check_in_reminder_msg']").parent().hide();
-        $("label[for='automatic_reminder_at']").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='check_in_reminder_msg']"));
+    arr.push($("label[for='automatic_reminder_at']"));
+    toggleParentContainers(arr, flag);
 }
 
 function toggleAppointmentFields(flag) {
-    if (flag) {
-        $("label[for='appointment_duration']").parent().show();
-        $("label[for='office_email_paragraph'").parent().show();
-        $("label[for='online_status'").parent().show();
-    } else {
-        $("label[for='appointment_duration']").parent().hide();
-        $("label[for='office_email_paragraph'").parent().hide();
-        $("label[for='online_status'").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='appointment_duration']"));
+    arr.push($("label[for='office_email_paragraph']"));
+    arr.push($("label[for='online_status']"));
+    toggleParentContainers(arr, flag);
 }
 
 function toggleOnlineStatusFields(flag) {
-    if (flag) {
-        $("label[for='office_appointment_message']").parent().show();
-        $("label[for='civic_address']").parent().show();
-        $("label[for='telephone']").parent().show();
-        $("label[for='external_map_link']").parent().show();
-        $("label[for='appointments_days_limit']").parent().show();
-        $("label[for='soonest_appointment']").parent().show();
-        $("label[for='max_person_appointment_per_day']").parent().show();
-        $("label[for='number_of_dlkt']").parent().show();
-    } else {
-        $("label[for='office_appointment_message']").parent().hide();
-        $("label[for='civic_address']").parent().hide();
-        $("label[for='telephone']").parent().hide();
-        $("label[for='external_map_link']").parent().hide();
-        $("label[for='appointments_days_limit']").parent().hide();
-        $("label[for='soonest_appointment']").parent().hide();
-        $("label[for='max_person_appointment_per_day']").parent().hide();
-        $("label[for='number_of_dlkt']").parent().hide();
-    }
+    var arr = [];
+    arr.push($("label[for='office_appointment_message']"));
+    arr.push($("label[for='civic_address']"));
+    arr.push($("label[for='telephone']"));
+    arr.push($("label[for='external_map_link']"));
+    arr.push($("label[for='appointments_days_limit']"));
+    arr.push($("label[for='soonest_appointment']"));
+    arr.push($("label[for='max_person_appointment_per_day']"));
+    arr.push($("label[for='number_of_dlkt']"));
+    toggleParentContainers(arr, flag);
 }
