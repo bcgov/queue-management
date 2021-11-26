c6b84496cb2e35657f95ece0ab362af1ff6f495f
Chris Gabel <Chris.Gabel@gov.bc.ca>
2021-11-25 17:24:48 -0800

    Reverted keycloak.js file changes (6 code smells)

--- frontend/public/static/keycloak.js
+++ frontend/public/static/keycloak.js
@@ -33,9 +33,9 @@
     }
 
     const scripts = document.getElementsByTagName('script')
-    for (let script of scripts) {
-      if ((script.src.indexOf('keycloak.js') !== -1 || script.src.indexOf('keycloak.min.js') !== -1) && script.src.indexOf('version=') !== -1) {
-        kc.iframeVersion = script.src.substring(script.src.indexOf('version=') + 8).split('&')[0]
+    for (let i = 0; i < scripts.length; i++) {
+      if ((scripts[i].src.indexOf('keycloak.js') !== -1 || scripts[i].src.indexOf('keycloak.min.js') !== -1) && scripts[i].src.indexOf('version=') !== -1) {
+        kc.iframeVersion = scripts[i].src.substring(scripts[i].src.indexOf('version=') + 8).split('&')[0]
       }
     }
 
@@ -320,7 +320,10 @@
     }
 
     kc.createLogoutUrl = function (options) {
-      return kc.endpoints.logout() + '?redirect_uri=' + encodeURIComponent(adapter.redirectUri(options, false))
+      const url = kc.endpoints.logout() +
+                '?redirect_uri=' + encodeURIComponent(adapter.redirectUri(options, false))
+
+      return url
     }
 
     kc.register = function (options) {
@@ -734,9 +737,9 @@
         if (!oidcProvider) {
           if (!config.url) {
             const scripts = document.getElementsByTagName('script')
-            for (let script of scripts) {
-              if (script.src.match(/.*keycloak\.js/)) {
-                config.url = script.src.substr(0, script.src.indexOf('/js/keycloak.js'))
+            for (let i = 0; i < scripts.length; i++) {
+              if (scripts[i].src.match(/.*keycloak\.js/)) {
+                config.url = scripts[i].src.substr(0, scripts[i].src.indexOf('/js/keycloak.js'))
                 break
               }
             }
@@ -883,10 +886,16 @@
       s[14] = '4'
       s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1)
       s[8] = s[13] = s[18] = s[23] = '-'
-      return s.join('')
+      const uuid = s.join('')
+      return uuid
     }
 
     kc.callback_id = 0
+
+    function createCallbackId () {
+      const id = '<id: ' + (kc.callback_id++) + (Math.random()) + '>'
+      return id
+    }
 
     function parseCallback (url) {
       const oauth = parseCallbackUrl(url)
@@ -968,15 +977,15 @@
         paramsString: '',
         oauthParams: {}
       }
-      for (let param of p) {
-        const t = param.split('=')
+      for (let i = 0; i < p.length; i++) {
+        const t = p[i].split('=')
         if (supportedParams.indexOf(t[0]) !== -1) {
           result.oauthParams[t[0]] = t[1]
         } else {
           if (result.paramsString !== '') {
             result.paramsString += '&'
           }
-          result.paramsString += param
+          result.paramsString += p[i]
         }
       }
       return result
@@ -1501,13 +1510,13 @@
       var getCookie = function (key) {
         const name = key + '='
         const ca = document.cookie.split(';')
-        for (let c of ca) {
-          let cookie = c
-          while (cookie.charAt(0) == ' ') {
-            cookie = cookie.substring(1)
+        for (let i = 0; i < ca.length; i++) {
+          let c = ca[i]
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1)
           }
-          if (cookie.indexOf(name) == 0) {
-            return cookie.substring(name.length, cookie.length)
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length)
           }
         }
         return ''
