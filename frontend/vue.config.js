4cffc317edd144c960e845c60486630deed3252f
tyu-avo <thomas.yu@avocette.com>
2022-03-23 22:23:35 +0000

    vue-cli upgrade to 5 from 4. Compiles without warnings, but runtime errors encountered

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,13 +6,28 @@ module.exports = {
       alias: {
         vue: path.resolve('./node_modules/vue'),
         $assets: path.resolve('./src/assets/')
+      },
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        buffer: require.resolve('buffer/'),
+        util: require.resolve('util/'),
+        stream: require.resolve('stream-browserify')
       }
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .set('parser', {
+        dataUrlCondition: {
+          maxSize: 4 * 1024 // 4KiB
+        }
+      })
   },
   // publicPath: process.env.VUE_APP_PATH,
   runtimeCompiler: true,
 
   // Necessary for IE11 compat
-  transpileDependencies: ['vuetify'],
+  transpileDependencies: ['vuetify']
 
 }
