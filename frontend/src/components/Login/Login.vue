41c6c80e9fc7e36839e54a5c86edb55da9502a7b
Raj Ahuja <Raj.Ahuja@avocette.com>
2024-02-26 20:05:24 +0000

    Revert changes

--- frontend/src/components/Login/Login.vue
+++ frontend/src/components/Login/Login.vue
@@ -290,8 +290,8 @@ export default class Login extends Vue {
   }
 
   login () {
-    // this.$keycloak.login({ idpHint: 'idir', scope: 'offline_access' })
-    this.$keycloak.login({ idpHint: 'idir' }) // Removed scope: 'offline_access'
+    this.$keycloak.login({ idpHint: 'idir', scope: 'offline_access' })
+    // this.$keycloak.login({ idpHint: 'idir' }) // Removed scope: 'offline_access'
   }
 
   logoutTokenExpired () {
