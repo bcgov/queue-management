7f7b99a70f63dd3ef6f42d36915fdba4dc4ab01e
dinesh <dinesh.pb@aot-technologies.com>
2024-03-07 22:37:11 -0800

    select office in appointments screen

--- frontend/src/components/Appointments/buttons-appointments.vue
+++ frontend/src/components/Appointments/buttons-appointments.vue
@@ -1,51 +1,43 @@
 <template>
   <b-container>
-    <div class="full-div">
-      <div>
+    <div class="row">
+      <div class="col-5">
         <form inline>
           <b-button class="btn-primary mx-2" @click="today">Today</b-button>
           <b-button class="btn-primary mx-2" v-if="toggleAppCalenderView" @click="agendaWeek">Week View</b-button>
           <b-button class="btn-primary mx-2" v-if="!toggleAppCalenderView" @click="agendaDay"> Day View</b-button>
-          <b-button
-            variant="primary"
-            class="ml-0 mx-2"
-            @click="clickBlackout"
-            >Create Blackout</b-button
-          >
-          <b-button
-      variant="primary"
-      class="ml-0 mx-2 fl-right"
-      @click="clickNewApp"
-      >
-      <font-awesome-icon
-        icon="plus"
-        class="m-0 p-0"
-        style="font-size: 1rem"
-    />
-      New Appointment
-    </b-button>
+          <b-button variant="primary" class="ml-0 mx-2" @click="clickBlackout">Create Blackout</b-button>
         </form>
       </div>
-      <div class="align-center-text">
-        <span class="q-inline-title">
-          {{ calendar_setup.title }}
-          {{ calendar_setup.titleRef && calendar_setup.titleRef.title }}
-        </span>
+      <div class="col-1">
+        <label class="mb-0">Choose Branch</label>
       </div>
-      <div class="align-center-arrow">
+      <div class="col-3">
+        <b-form-select  :value="selected_office_id" :options="officeList" @change="onChangeOffice">
+        </b-form-select>
+      </div>
+      <div class="col-3">
+        <b-button variant="primary" class="ml-0 mx-2 fl-right" @click="clickNewApp">
+          <font-awesome-icon icon="plus" class="m-0 p-0" style="font-size: 1rem" />
+          New Appointment
+        </b-button>
+      </div>
+    </div>
+    <div class="row" style="text-align: center;">
+      <span class="col-12 q-inline-title">
+        {{ calendar_setup.title }}
+        {{ calendar_setup.titleRef && calendar_setup.titleRef.title }}
+      </span>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="align-center-arrow">
           <b-button class="btn-primary" @click="prev">
-            <font-awesome-icon
-              icon="angle-left"
-              class="m-0 p-0"
-              style="font-size: 1rem"
-          /> Last</b-button>
+            <font-awesome-icon icon="angle-left" class="m-0 p-0" style="font-size: 1rem" /> Last</b-button>
           <b-button class="btn-primary ml-1" @click="next">
             Next
-            <font-awesome-icon
-              icon="angle-right"
-              class="m-0 p-0"
-              style="font-size: 1rem"
-          /></b-button>
+            <font-awesome-icon icon="angle-right" class="m-0 p-0" style="font-size: 1rem" /></b-button>
+        </div>
       </div>
     </div>
   </b-container>
@@ -54,12 +46,11 @@
 <script lang="ts">
 /* eslint-disable */
 import { Component, Vue } from 'vue-property-decorator'
-import { namespace } from 'vuex-class'
+import { namespace, State } from 'vuex-class'
 
 import ApptBookingModal from './appt-booking-modal/appt-booking-modal.vue'
 
 const appointmentsModule = namespace('appointmentsModule')
-
 @Component({
   components: {
     ApptBookingModal
@@ -83,11 +74,15 @@ export default class ButtonsAppointments extends Vue {
   @appointmentsModule.Mutation('setAgendaClickedTime') public setAgendaClickedTime: any
   @appointmentsModule.Mutation('toggleApptBookingModal') public toggleApptBookingModal: any
 
-  
+
   @appointmentsModule.State('toggleAppCalenderView') public toggleAppCalenderView: any
   @appointmentsModule.Mutation('setToggleAppCalenderView') public setToggleAppCalenderView: any
 
-  get dropdownText () {
+  @appointmentsModule.Getter('getSelectedOfficeId') public selected_office_id!: any;
+  @appointmentsModule.Mutation('setAppointmentsOfficeId') public setAppointmentsOfficeId: any
+  @appointmentsModule.Action('getAppointments') private getAppointments: any;
+  @State('offices') private offices!: any;
+  get dropdownText() {
     if (this.calendar_setup.name === 'day') {
       return 'Day View'
     }
@@ -97,51 +92,68 @@ export default class ButtonsAppointments extends Vue {
     return 'Week View'
   }
 
-  agendaDay () {
+  agendaDay() {
     this.$root.$emit('agendaDay')
     this.setToggleAppCalenderView(true)
   }
 
-  agendaWeek () {
+  agendaWeek() {
     this.$root.$emit('agendaWeek')
     this.setToggleAppCalenderView(false)
   }
 
-  next () {
+  next() {
     this.$root.$emit('next')
   }
 
-  prev () {
+  prev() {
     this.$root.$emit('prev')
   }
 
-  today () {
+  today() {
     this.$root.$emit('today')
   }
 
-  clickBlackout () {
+  clickBlackout() {
     this.toggleAppointmentBlackoutModal(true)
   }
 
-  clickNewApp () {
+  clickNewApp() {
     this.setAgendaClickedTime(null)
     this.setAgendaClickedAppt(null)
     this.toggleApptBookingModal(true)
   }
+  get officeList() {
+    return this.offices.map(p => ({ text: p.office_name, value: p.office_id }))
+  }
+  onChangeOffice(office_id) {
+    this.setAppointmentsOfficeId(office_id)
+    console.log("Selected Office Id ", this.selected_office_id);
+    this.getAppointments();
+  }
+  mounted(){
+    this.getAppointments();
+    this.setAppointmentsOfficeId(this.$store.state.user.office_id)
+    console.log("Onload Office Id ", this.selected_office_id);
+  }
 }
 </script>
+
 <style scoped>
 .align-center-text {
   padding-left: 40%;
   margin-top: -46px;
 }
+
 .full-div {
   width: 100%;
 }
-.align-center-arrow { 
+
+.align-center-arrow {
   padding-left: 48%;
 }
-.fl-right{
+
+.fl-right {
   float: right;
 }
 </style>
