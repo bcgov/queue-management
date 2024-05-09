c30094e1b1056604bb73ef64df845a2c0ff982fb
Raj Ahuja <Raj.Ahuja@avocette.com>
2024-05-09 17:21:52 +0000

    Log data

--- api/app/resources/bookings/exam/exam_post.py
+++ api/app/resources/bookings/exam/exam_post.py
@@ -63,6 +63,7 @@ class ExamPost(Resource):
             formatted_data = self.format_data(json_data, exam)
             exam = formatted_data["exam"]
             job = self.bcmp_service.check_exam_status(exam)
+            logging.warning("Test job: %s", job)
             my_print(job)
             #if job and job['jobProperties'] and job['jobProperties']['JOB_ID']:
                 #exam.event_id = job['jobProperties']['JOB_ID']
