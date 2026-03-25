fa3abd22353643e3ad0c3154a6e99c80da820048
Chris Sampson <chris.sampson@gov.bc.ca>
2026-03-24 23:45:09 -0700

    Remove servicenow feedback

--- api/app/resources/theq/feedback.py
+++ api/app/resources/theq/feedback.py
@@ -19,7 +19,6 @@ import json
 import urllib.request
 import urllib.parse
 import os
-import pysnow
 import requests
 import json
 from app.utilities.auth_util import Role, has_any_role
@@ -31,7 +30,6 @@ class Feedback(Resource):
 
     feedback_destinations = application.config['THEQ_FEEDBACK']
     flag_teams = "TEAMS" in feedback_destinations
-    flag_service_now = "SERVICENOW" in feedback_destinations
     flag_rocket_chat = "ROCKETCHAT" in feedback_destinations
 
     @jwt.has_one_of_roles([Role.internal_user.value])
@@ -46,26 +44,21 @@ class Feedback(Resource):
             return {"message": "Must provide message to send as feedback"}, 422
 
         teams_result = None
-        service_now_result = None
         rocket_chat_result = None
 
         if self.flag_teams:
             teams_result = Feedback.send_to_teams(feedback_message)
 
-        if self.flag_service_now:
-            service_now_result = Feedback.send_to_service_now(feedback_message)
-
         if self.flag_rocket_chat:
             rocket_chat_result = Feedback.send_to_rocket_chat(feedback_message)
 
-        #  Calculate return message as combination of teams and service now results.
-        result = Feedback.combine_results("Teams: ", teams_result, "Service Now: ", service_now_result)
-        result = Feedback.combine_results("", result, "Rocket Chat: ", rocket_chat_result)
+        #  Calculate return message as combination
+        result = Feedback.combine_results("Teams: ", teams_result, "Rocket Chat: ", rocket_chat_result)
         return result
 
     @staticmethod
     def send_to_teams(feedback_message):
-        
+
         url = application.config['TEAMS_URL']
 
         if url is None:
@@ -89,76 +82,8 @@ class Feedback(Resource):
             return {"message": "error", "http_code": resp.getcode()}, 400
 
     @staticmethod
-    def send_to_service_now(params):
-        
-        instance = application.config['SERVICENOW_INSTANCE']
-        user = application.config['SERVICENOW_USER']
-        password = application.config['SERVICENOW_PASSWORD']
-        table = application.config['SERVICENOW_TABLE']
-        tenant = application.config['SERVICENOW_TENANT']
-        assign_group = application.config['SERVICENOW_ASSIGN_GROUP']
-
-        if instance is None:
-            return {"message": "SERVICENOW_INSTANCE is not set"}, 400
-        if user is None:
-            return {"message": "SERVICENOW_USER is not set"}, 400
-        if password is None:
-            return {"message": "SERVICENOW_PASSWORD is not set"}, 400
-        if table is None:
-            return {"message": "SERVICENOW_TABLE is not set"}, 400
-        if tenant is None:
-            return {"message": "SERVICENOW_TENANT is not set"}, 400
-        if assign_group is None:
-            return {"message": "SERVICENOW_ASSIGN_GROUP is not set"}, 400
-
-        #  Generate Service Now incident.
-        #  NOTE:  Automatic email gets sent to the assignment group below
-        #         ONLY IF the email of the SERVICENOW_USER IS NOT the same
-        #         as the email of this assignment group.
-
-        c = pysnow.Client(instance = instance, user=user, password=password)
-        incident = c.resource(api_path=table)
-        csr = Feedback.extract_string(params, "Username: ", "\n", 0)
-        ticket = Feedback.extract_string(params, "Ticket Number: ","\n", 0)
-        msg = Feedback.extract_string(params, "Message: ", "", 50)
-        short_desc = "TheQ Feedback (CSR: " + csr + "; Ticket: " + ticket + "; Msg: " + msg + ")"
-
-        #  Create new record depending on tenant value.
-        if len(tenant) != 0:
-          new_record = {
-              'caller_id': user,
-              'category': 'Inquiry / Help',
-              'cmdb_ci': 'CFMS',
-              'u_tenant': tenant,
-              'impact': '2 - Some Customers',
-              'urgency': '2 - High',
-              'priority': 'High',
-              'short_description': short_desc,
-              'description': params,
-              'assignment_group': assign_group
-          }
-        else:
-            new_record = {
-                'category': 'Inquiry / Help',
-                'cmdb_ci': 'CFMS',
-                'impact': '2 - Some Customers',
-                'urgency': '2 - High',
-                'priority': 'High',
-                'short_description': short_desc,
-                'description': params,
-                'assignment_group': assign_group
-            }
-
-        result = incident.create(payload=new_record)
-
-        if '201' in str(result):
-            return {"status": "Success"}, 201
-        else:
-            return {"message": "Service Now incident not created"}, 400
-
-    @staticmethod
     def send_to_rocket_chat(feedback_message):
-        
+
         url = application.config['ROCKET_CHAT_URL']
 
         if url is None:
