4b138566be2ba88a588ac494daf489cd402a9024
Veenu Punyani <veenucan24@gmail.com>
2026-05-13 15:08:54 -0700

    dev-44: add intial message to homepage

--- appointment-booking/src/App.tsx
+++ appointment-booking/src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div id="app" />
+  return (
+    <div id="app">
+      <h1>Appointment Booking</h1>
+      <p>React boilerplate ready for development</p>
+    </div>
+  )
 }
 
 export default App
