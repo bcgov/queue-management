2a3fe5fdc2e5187c67e7a9b8116ac15e3e8fbda4
Veenu Punyani <veenucan24@gmail.com>
2026-06-18 18:23:12 -0700

    Add prerender script for locations directory and run it in build
    Added  scripts/prerender-locations.tsx to pre-render a static Service BC locations page and copy required CSS/fonts into public/.
    
    Updated .gitignore to exclude generated public/locations and related assets.
    
    Refactored src/index.css with layout and locations table styles to support the pre-rendered page.

--- appointment-booking/eslint.config.js
+++ appointment-booking/eslint.config.js
@@ -8,7 +8,7 @@ import prettierPlugin from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'scripts']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
