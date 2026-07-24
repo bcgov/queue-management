11250cad32c2e9fa27ec41f70639c81ec6c1664f
Chris Sampson <chris.sampson@gov.bc.ca>
2026-07-24 12:58:06 -0700

    Add API endpoint for offices by service ID

--- api/app/resources/theq/offices.py
+++ api/app/resources/theq/offices.py
@@ -12,12 +12,72 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.'''
 
+import datetime
 import logging
-from flask import g
+
+from flask import request
 from flask_restx import Resource
 from qsystem import api, db
 from sqlalchemy import exc
-from app.models.theq import CSR, Office
+from app.models.theq import Office, Service
+from app.schemas.theq import OfficeSchema
+from app.services import AvailabilityService
+from app.utilities.timezone_utils import get_timezone
+
+
+@api.route("/offices", methods=["GET"])
+class OfficesByService(Resource):
+
+    @staticmethod
+    def _get_next_appointment_date(office, service):
+        timezone = get_timezone(office.timezone.timezone_name)
+        today = datetime.datetime.now().astimezone(timezone)
+        days = [
+            today + datetime.timedelta(days=day)
+            for day in range(office.appointments_days_limit or 0)
+        ]
+
+        if not days:
+            return None
+
+        available_slots = AvailabilityService.get_available_slots(
+            office=office, days=days, service=service
+        )
+        for day in days:
+            if available_slots.get(day.strftime('%m/%d/%Y')):
+                return day.date().isoformat()
+
+        return None
+
+    def get(self):
+        try:
+            service_id = int(request.args.get('service_id'))
+        except (TypeError, ValueError):
+            return {'message': 'service_id must be an integer.'}, 400
+
+        try:
+            service = db.session.get(Service, service_id)
+            if service is None:
+                return {'offices': [],
+                        'errors': {}}
+
+            offices = Office.query.join(Office.services).filter(
+                Service.service_id == service_id,
+                Office.deleted.is_(None)
+            ).order_by(Office.office_name).all()
+            result = OfficeSchema(many=True).dump(offices)
+
+            for office, serialized_office in zip(offices, result):
+                serialized_office['next_appointment_date'] = self._get_next_appointment_date(
+                    office, service
+                )
+
+            return {'offices': result,
+                    'errors': {}}
+
+        except exc.SQLAlchemyError as exception:
+            logging.exception(exception)
+            return {'message': 'API is down'}, 500
 
 
 @api.route("/offices/", methods=["GET"])
